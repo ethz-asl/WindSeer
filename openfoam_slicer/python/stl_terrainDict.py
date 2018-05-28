f3c20220bd7bb896a74e7cb24471f70f9bc3711e
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2018-05-29 00:01:52 +1000

    Updating slicer

--- openfoam_slicer/python/stl_terrainDict.py
+++ openfoam_slicer/python/stl_terrainDict.py
@@ -6,7 +6,7 @@ from stl import mesh
 from string import Template
 
 
-def create_blockMeshDict(outfile, xyz_lims, nx=10, ny=10, nz=10, infile = './blockMeshDict.in',
+def create_terrainDict(outfile, xyz_lims, nx=10, ny=10, nz=10, infile = './terrainDict.in',
                          mconvert=1.0, in_buffer=0.0, gx=1, gy=1, gz=1, quiet=False):
     xyz_lims = np.array(xyz_lims)
     dx, dy, dz = [h - l for l, h in xyz_lims]
@@ -31,10 +31,10 @@ def create_blockMeshDict(outfile, xyz_lims, nx=10, ny=10, nz=10, infile = './blo
         out_fh.write(mesh_dict)
 
 
-def generate_blockMeshDict(stl_file, block_mesh, infile='blockMeshDict.in', nx=128, ny=128, nz=128, pad_z=3.0):
+def generate_terrainDict(stl_file, block_mesh, infile='terrainDict.in', nx=128, ny=128, nz=128, pad_z=3.0):
 
-    if os.path.basename(block_mesh) is not 'blockMeshDict':
-        print "Warning: Specified output \"{0}\" should be a blockMeshDict file".format(block_mesh)
+    if os.path.basename(block_mesh) is not 'terrainDict':
+        print "Warning: Specified output \"{0}\" should be a terrainDict file".format(block_mesh)
 
     hill_mesh = mesh.Mesh.from_file(stl_file)
 
@@ -48,14 +48,14 @@ def generate_blockMeshDict(stl_file, block_mesh, infile='blockMeshDict.in', nx=1
     lims[2, 1] = lims[2, 0] + pad_z*(lims[2, 1] - lims[2, 0])
 
     bmesh_extras = {'nx': nx, 'ny': ny, 'nz': nz, 'infile': infile}
-    create_blockMeshDict(block_mesh, lims, **bmesh_extras)
+    create_terrainDict(block_mesh, lims, **bmesh_extras)
     return lims
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Generate blockMeshDict from stl file')
+    parser = argparse.ArgumentParser(description='Generate terrainDict from stl file')
     parser.add_argument('-s', '--stl', required=True, help='Input stl file')
     parser.add_argument('-o', '--block-mesh-out', required=True, help='Output block mesh file')
-    parser.add_argument('-in', '--block-mesh-in', default='blockMeshDict.in', help='Input block mesh file (usually .in)')
+    parser.add_argument('-in', '--block-mesh-in', default='terrainDict.in', help='Input block mesh file (usually .in)')
     parser.add_argument('-nx', type=int, default=128,
                         help='Number of points in x direction (uniform)')
     parser.add_argument('-ny', type=int, default=128,
@@ -66,4 +66,4 @@ if __name__ == "__main__":
     parser.add_argument('-L', )
     args = parser.parse_args()
 
-    generate_blockMeshDict(args.stl, args.block_mesh_out, args.block_mesh_in, nx=args.nx, ny=args.ny, nz=args.nz, pad_z=args.pad_z)
+    generate_terrainDict(args.stl, args.block_mesh_out, args.block_mesh_in, nx=args.nx, ny=args.ny, nz=args.nz, pad_z=args.pad_z)
