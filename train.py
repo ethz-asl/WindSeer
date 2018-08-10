8c0db1e783b4701c2da2fb6513c5c17745fa4a05
acfloria <florian.achermann@mavt.ethz.ch>
2018-08-10 13:50:45 +0200

    Adopt training script for changes in 3D model

--- train.py
+++ train.py
@@ -11,9 +11,9 @@ import utils
 
 # ---- Params --------------------------------------------------------------
 # learning parameters
-learning_rate = 1e-3
+learning_rate = 1e-1
 plot_every_n_batches = 10
-n_epochs = 1
+n_epochs = 10
 batchsize = 32
 num_workers = 8
 
@@ -21,28 +21,45 @@ num_workers = 8
 save_model = True
 save_learning_curve = True
 evaluate_testset = True
-warm_start = False
+warm_start = True
 custom_loss = False
 
 # dataset parameter
-trainset_name = 'data/converted_train_new_boolean.tar'
-validationset_name = 'data/converted_validation_new_boolean.tar'
-testset_name = 'data/converted_test_new_boolean.tar'
+trainset_name = 'data/converted_3d.tar'
+validationset_name = 'data/converted_3d.tar'
+testset_name = 'data/converted_3d.tar'
 stride_hor = 2
 stride_vert = 1
-
-# model parameter
-model_name = 'ednn_2D_scaled_nearest_skipping_new_boolean'
-uhor_scaling = 9.0
+uhor_scaling = 6.0
 uz_scaling = 2.5
 turbulence_scaling = 4.5
-use_turbulence = False
+
+# model parameter
+d3 = True
+model_name = 'ednn_3D_scaled_nearest_skipping_boolean'
+n_input_layers = 4
+n_output_layers = 4
+n_x = 64
+n_y = 64
+n_z = 64
+n_downsample_layers = 5
 interpolation_mode = 'nearest'
 align_corners = False
-number_input_layers = 3
 skipping = True
-d3 = False
+use_terrain_mask = True
+pooling_method = 'maxpool'
 # --------------------------------------------------------------------------
+
+if d3:
+    if n_input_layers > 3:
+        use_turbulence = True
+    else:
+        use_turbulence = False
+else:
+    if n_input_layers > 2:
+        use_turbulence = True
+    else:
+        use_turbulence = False
 
 # define dataset and dataloader
 trainset = utils.MyDataset(trainset_name, stride_hor = stride_hor, stride_vert = stride_vert, turbulence_label = use_turbulence, scaling_uhor = uhor_scaling, scaling_uz = uz_scaling, scaling_nut = turbulence_scaling)
@@ -57,33 +74,12 @@ validationloader = torch.utils.data.DataLoader(validationset, batch_size=1,
 
 #check if gpu is available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print('-----------------------------------------------------------------------------')
-print('INFO: Start training on device %s' % device)
-print(' ')
-print('Train Settings:')
-print('\tWarm start:\t\t', warm_start)
-print('\tLearning rate:\t\t', learning_rate)
-print('\tBatchsize:\t\t', batchsize)
-print('\tEpochs:\t\t\t', n_epochs)
-print(' ')
-print('Model Settings:')
-print('\tModel name:\t\t', model_name)
-print('\t3D:\t\t\t', d3)
-print('\tUse turbulence:\t\t', use_turbulence)
-print('\tInterpolation mode:\t', interpolation_mode)
-print('\tAlign corners:\t\t', align_corners)
-print('\tNumber of inputs:\t', number_input_layers)
-print('\tSkip connection:\t', skipping)
-print('\tUhor scaling:\t\t', uhor_scaling)
-print('\tUz scaling:\t\t', uz_scaling)
-print('\tTurbulence scaling:\t', turbulence_scaling)
-print('-----------------------------------------------------------------------------')
 
 # define model and move to gpu if available
 if d3:
-    net = models.ModelEDNN3D(number_input_layers, interpolation_mode = interpolation_mode, align_corners = align_corners, skipping = skipping, predict_turbulence = use_turbulence)
+    net = models.ModelEDNN3D(n_input_layers, n_output_layers, n_x, n_y, n_z, n_downsample_layers, interpolation_mode, align_corners, skipping, use_terrain_mask, pooling_method)
 else:
-    net = models.ModelEDNN2D(number_input_layers, interpolation_mode = interpolation_mode, align_corners = align_corners, skipping = skipping, predict_turbulence = use_turbulence)
+    net = models.ModelEDNN2D(n_input_layers, interpolation_mode = interpolation_mode, align_corners = align_corners, skipping = skipping, predict_turbulence = use_turbulence)
 
 if (warm_start):
     try:
@@ -106,6 +102,38 @@ else:
 
 # initialize variable to store the learning curve
 learning_curve = np.zeros([n_epochs, 2])
+
+print('-----------------------------------------------------------------------------')
+print('INFO: Start training on device %s' % device)
+print(' ')
+print('Train Settings:')
+print('\tWarm start:\t\t', warm_start)
+print('\tLearning rate:\t\t', learning_rate)
+print('\tBatchsize:\t\t', batchsize)
+print('\tEpochs:\t\t\t', n_epochs)
+print(' ')
+print('Model Settings:')
+print('\tModel name:\t\t', model_name)
+print('\t3D:\t\t\t', d3)
+print('\tNumber of inputs:\t', n_input_layers)
+print('\tNumber of outputs:\t', n_output_layers)
+print('\tNx:\t\t\t', n_x)
+print('\tNy:\t\t\t', n_y)
+print('\tNz:\t\t\t', n_z)
+print('\tNumber conv layers:\t', n_downsample_layers)
+print('\tInterpolation mode:\t', interpolation_mode)
+print('\tAlign corners:\t\t', align_corners)
+print('\tSkip connection:\t', skipping)
+print('\tUse terrain mask:\t', use_terrain_mask)
+print('\tPooling method:\t\t', pooling_method)
+print(' ')
+print('Dataset Settings:')
+print('\tUhor scaling:\t\t', uhor_scaling)
+print('\tUz scaling:\t\t', uz_scaling)
+print('\tTurbulence scaling:\t', turbulence_scaling)
+print('\tHorizontal stride:\t', stride_hor)
+print('\tVertical stride:\t', stride_vert)
+print('-----------------------------------------------------------------------------')
 
 # start the training for n_epochs
 start_time = time.time()
@@ -171,14 +199,20 @@ if (save_model):
 
     # save the model parameters
     model_params = {
+        'n_input_layers': n_input_layers,
+        'n_output_layers': n_output_layers,
+        'n_x': n_x,
+        'n_y': n_y,
+        'n_z': n_z,
+        'n_downsample_layers': n_downsample_layers,
+        'interpolation_mode': interpolation_mode,
+        'align_corners': align_corners,
+        'skipping': skipping,
+        'use_terrain_mask': use_terrain_mask,
+        'pooling_method': pooling_method,
         'uhor_scaling': uhor_scaling,
         'uz_scaling': uz_scaling,
         'turbulence_scaling': turbulence_scaling,
-        'interpolation_mode': interpolation_mode,
-        'align_corners': align_corners,
-        'number_input_layers': number_input_layers,
-        'skipping': skipping,
-        'use_turbulence': use_turbulence,
         'stride_hor': stride_hor,
         'stride_vert': stride_vert
         }
