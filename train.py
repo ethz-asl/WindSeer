f1852c53813fcb23293824b52be32e1d5bb299c7
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2018-08-17 08:48:47 +0200

    Added yaml reader for config files

--- train.py
+++ train.py
@@ -10,109 +10,66 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import StepLR
 import utils
+import time
+import argparse
+import os
 
-# ---- Params --------------------------------------------------------------
-# learning parameters
-plot_every_n_batches = 10
-n_epochs = 10000
-batchsize = 8
-num_workers = 8
-learning_rate_initial = 5e-4
-learning_rate_decay = 0.5
-learning_rate_decay_step_size = 2500
-compute_validation_loss = False
-custom_init = False
+now_time = time.strftime("%Y_%m_%d-%H_%M")
 
-# options to store data
-save_model = True
-save_metadata = True
-evaluate_testset = False
-warm_start = False
-custom_loss = False
-save_model_every_n_epoch = 100000
-save_params_hist_every_n_epoch = 100
+parser = argparse.ArgumentParser(description='Training an EDNN for predicting wind data from terrain')
+parser.add_argument('-np', '--no-plots', dest='make_plots', action='store_false', help='Turn off plots (default False)')
+parser.add_argument('-y', '--yaml-config', required=True, help='YAML config file')
+parser.add_argument('-o', '--output-dir', default='models/trained_models/', help='Output directory')
 
-# dataset parameter
-trainset_name = 'data/converted_3d_one.tar'
-validationset_name = 'data/converted_3d_one.tar'
-testset_name = 'data/converted_3d_one.tar'
-stride_hor = 4
-stride_vert = 2
-uhor_scaling = 6.0
-uz_scaling = 2.5
-turbulence_scaling = 4.5
+args = parser.parse_args()
 
-# model parameter
-d3 = True
-model_name = 'ednn_3D_n_sb4smf1_r_one'
-n_input_layers = 4
-n_output_layers = 3
-n_x = 32
-n_y = 32
-n_z = 32
-n_downsample_layers = 4
-interpolation_mode = 'nearest'
-align_corners = False
-skipping = True
-use_terrain_mask = True
-pooling_method = 'striding'
-use_fc_layers = True
-fc_scaling = 1
-use_mapping_layer = False
+run_params = utils.EDNNParameters(args.yaml_config)
+model_dir = os.path.join(args.output_dir, run_params.name)
+if run_params.run['save_model'] and (not os.path.exists(model_dir)):
+    os.mkdir(model_dir)
 # --------------------------------------------------------------------------
 
-# decide if turbulence is used (somewhat a hack maybe there is something better in the future)
-if d3:
-    if n_output_layers > 3:
-        use_turbulence = True
-    else:
-        use_turbulence = False
-else:
-    if n_output_layers > 2:
-        use_turbulence = True
-    else:
-        use_turbulence = False
+
 
 # define dataset and dataloader
-trainset = utils.MyDataset(trainset_name, stride_hor = stride_hor, stride_vert = stride_vert, turbulence_label = use_turbulence, scaling_uhor = uhor_scaling, scaling_uz = uz_scaling, scaling_nut = turbulence_scaling)
+trainset = utils.MyDataset(run_params.data['trainset_name'], **run_params.MyDataset_kwargs())
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batchsize,
-                                          shuffle=True, num_workers=num_workers)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=run_params.run['batchsize'],
+                                          shuffle=True, num_workers=run_params.run['num_workers'])
 
-validationset = utils.MyDataset(validationset_name, stride_hor = stride_hor, stride_vert = stride_vert, turbulence_label = use_turbulence, scaling_uhor = uhor_scaling, scaling_uz = uz_scaling, scaling_nut = turbulence_scaling)
+validationset = utils.MyDataset(run_params.data['validationset_name'], **run_params.MyDataset_kwargs())
 
-validationloader = torch.utils.data.DataLoader(validationset, batch_size=1,
-                                          shuffle=False, num_workers=num_workers)
+validationloader = torch.utils.data.DataLoader(validationset, shuffle=False, batch_size=run_params.run['batchsize'],
+                                          num_workers=run_params.run['num_workers'])
 
 #check if gpu is available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # define model and move to gpu if available
-if d3:
-    net = models.ModelEDNN3D(n_input_layers, n_output_layers, n_x, n_y, n_z, n_downsample_layers, interpolation_mode, 
-                             align_corners, skipping, use_terrain_mask, pooling_method, use_mapping_layer,
-                             use_fc_layers, fc_scaling)
+if run_params.model['d3']:
+    net = models.ModelEDNN3D(**run_params.model3d_kwargs())
 else:
-    net = models.ModelEDNN2D(n_input_layers, interpolation_mode = interpolation_mode, align_corners = align_corners, skipping = skipping, predict_turbulence = use_turbulence)
+    net = models.ModelEDNN2D(**run_params.model2d_kwargs())
 
-if (warm_start):
+if (run_params.run['warm_start']):
     try:
-        net.load_state_dict(torch.load('models/trained_models/' + model_name + '.model', map_location=lambda storage, loc: storage))
+        net.load_state_dict(torch.load(os.path.join(model_dir, 'latest.model'), map_location=lambda storage, loc: storage))
     except:
         print('Warning: Failed to load the model parameter, initializing parameter.')
-        if custom_init:
+        if run_params.run['custom_init']:
             net.init_params()
 else:
-    if custom_init:
+    if run_params.run['custom_init']:
         net.init_params()
 
 net.to(device)
 
 # define optimizer and objective
-optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate_initial)
-scheduler = StepLR(optimizer, step_size=learning_rate_decay_step_size, gamma=learning_rate_decay)
+optimizer = torch.optim.Adam(net.parameters(), lr=run_params.run['learning_rate_initial'])
+scheduler = StepLR(optimizer, step_size=run_params.run['learning_rate_decay_step_size'],
+                   gamma=run_params.run['learning_rate_decay'])
 
-if custom_loss:
+if run_params.run['custom_loss']:
     loss_fn = utils.MyLoss(device)
     loss_fn_val = torch.nn.MSELoss()
 else:
@@ -120,75 +77,20 @@ else:
     loss_fn_val = torch.nn.MSELoss()
 
 # initialize the tensorboard writer
-writer = SummaryWriter('models/trained_models/' + model_name + '_learningcurve')
+writer = SummaryWriter(os.path.join(model_dir, 'learningcurve'))
 
 # save the model parameter in the beginning
-if (save_model):
-    model_params = {
-        'n_input_layers': n_input_layers,
-        'n_output_layers': n_output_layers,
-        'n_x': n_x,
-        'n_y': n_y,
-        'n_z': n_z,
-        'n_downsample_layers': n_downsample_layers,
-        'interpolation_mode': interpolation_mode,
-        'align_corners': align_corners,
-        'skipping': skipping,
-        'use_terrain_mask': use_terrain_mask,
-        'pooling_method': pooling_method,
-        'uhor_scaling': uhor_scaling,
-        'uz_scaling': uz_scaling,
-        'turbulence_scaling': turbulence_scaling,
-        'stride_hor': stride_hor,
-        'stride_vert': stride_vert,
-        'use_turbulence': use_turbulence,
-        'd3': d3,
-        'use_fc_layers': use_fc_layers,
-        'fc_scaling': fc_scaling,
-        'use_mapping_layer': use_mapping_layer
-        }
-    np.save('models/trained_models/' + model_name + '_params.npy', model_params)
+run_params.save(model_dir)
 
 print('-----------------------------------------------------------------------------')
 print('INFO: Start training on device %s' % device)
 print(' ')
-print('Train Settings:')
-print('\tWarm start:\t\t', warm_start)
-print('\tLearning rate initial:\t', learning_rate_initial)
-print('\tLearning rate step size:', learning_rate_decay_step_size)
-print('\tLearning rate decay:\t', learning_rate_decay)
-print('\tBatchsize:\t\t', batchsize)
-print('\tEpochs:\t\t\t', n_epochs)
-print(' ')
-print('Model Settings:')
-print('\tModel name:\t\t', model_name)
-print('\t3D:\t\t\t', d3)
-print('\tNumber of inputs:\t', n_input_layers)
-print('\tNumber of outputs:\t', n_output_layers)
-print('\tNx:\t\t\t', n_x)
-print('\tNy:\t\t\t', n_y)
-print('\tNz:\t\t\t', n_z)
-print('\tNumber conv layers:\t', n_downsample_layers)
-print('\tInterpolation mode:\t', interpolation_mode)
-print('\tAlign corners:\t\t', align_corners)
-print('\tSkip connection:\t', skipping)
-print('\tUse terrain mask:\t', use_terrain_mask)
-print('\tPooling method:\t\t', pooling_method)
-print('\tUse fc layers:\t\t', use_fc_layers)
-print('\tFC layer scaling:\t', fc_scaling)
-print('\tUse mapping layer:\t', use_mapping_layer)
-print(' ')
-print('Dataset Settings:')
-print('\tUhor scaling:\t\t', uhor_scaling)
-print('\tUz scaling:\t\t', uz_scaling)
-print('\tTurbulence scaling:\t', turbulence_scaling)
-print('\tHorizontal stride:\t', stride_hor)
-print('\tVertical stride:\t', stride_vert)
+run_params.print()
 print('-----------------------------------------------------------------------------')
 
 # start the training for n_epochs
 start_time = time.time()
-for epoch in range(n_epochs):  # loop over the dataset multiple times
+for epoch in range(run_params.run['n_epochs']):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
@@ -211,13 +113,16 @@ for epoch in range(n_epochs):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % plot_every_n_batches == (plot_every_n_batches - 1):    # print every plot_every_n_batches mini-batches
+
+        # print every plot_every_n_batches mini-batches
+        if i % run_params.run['plot_every_n_batches'] == (run_params.run['plot_every_n_batches'] - 1):
             print('[%d, %5d] averaged loss: %.5f' %
-                  (epoch + 1, i + 1, running_loss / (plot_every_n_batches - 1)))
+                  (epoch + 1, i + 1, running_loss / (run_params.run['plot_every_n_batches'] - 1)))
             running_loss = 0.0
 
-    if epoch % save_model_every_n_epoch == (save_model_every_n_epoch - 1):    # save model every save_model_every_n_epoch epochs
-        torch.save(net.state_dict(), 'models/trained_models/' + model_name + '_{}.model'.format(epoch+1))
+    # save model every save_model_every_n_epoch epochs
+    if epoch % run_params.run['save_model_every_n_epoch'] == (run_params.run['save_model_every_n_epoch'] - 1):
+        torch.save(net.state_dict(), os.path.join(model_dir, 'e{}.model'.format(epoch+1)))
 
     with torch.no_grad():
         train_loss = 0.0
@@ -230,7 +135,7 @@ for epoch in range(n_epochs):  # loop over the dataset multiple times
         train_loss /= len(trainloader)
 
         validation_loss = 0.0
-        if compute_validation_loss:
+        if run_params.run['compute_validation_loss']:
             for data in validationloader:
                 inputs, labels = data
                 inputs, labels = inputs.to(device), labels.to(device)
@@ -245,7 +150,7 @@ for epoch in range(n_epochs):  # loop over the dataset multiple times
         writer.add_scalar('Summary/ValidationLoss', validation_loss, epoch+1)
         writer.add_scalar('Summary/LearningRate', scheduler.get_lr()[0], epoch+1)
 
-        if epoch % save_params_hist_every_n_epoch == (save_params_hist_every_n_epoch - 1):
+        if epoch % run_params.run['save_params_hist_every_n_epoch'] == (run_params.run['save_params_hist_every_n_epoch'] - 1):
             for tag, value in net.named_parameters():
                 tag = tag.replace('.', '/')
                 writer.add_histogram(tag, value.data.cpu().numpy(), epoch+1)
@@ -257,18 +162,18 @@ for epoch in range(n_epochs):  # loop over the dataset multiple times
 
 print("INFO: Finished training in %s seconds" % (time.time() - start_time))
 
-if (save_model):
+if (run_params.run['save_model']):
     # save the model
-    torch.save(net.state_dict(), 'models/trained_models/' + model_name + '.model')
+    torch.save(net.state_dict(), os.path.join(model_dir, 'latest.model'))
 
 #writer.add_graph(net.cpu(), inputs.cpu()) # do not save the graph by default as the visualization does not work anyway that well
 writer.close()
 
 # evaluate the model performance on the testset if requested
-if (evaluate_testset):
-    testset = utils.MyDataset(testset_name, stride_hor = stride_hor, stride_vert = stride_vert, turbulence_label = use_turbulence, scaling_uhor = uhor_scaling, scaling_uz = uz_scaling, scaling_nut = turbulence_scaling)
+if (run_params.run['evaluate_testset']):
+    testset = utils.MyDataset(run_params.data['testset_name'], **run_params.MyDataset_kwargs())
     testloader = torch.utils.data.DataLoader(testset, batch_size=1,
-                                             shuffle=False, num_workers=num_workers)
+                                             shuffle=False, num_workers=run_params.data['num_workers'])
 
     with torch.no_grad():
         loss = 0.0
