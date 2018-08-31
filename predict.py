8d3a38c63d92488298d9036142ea9ded232ba8f9
acfloria <florian.achermann@mavt.ethz.ch>
2018-08-31 09:03:54 +0200

    Add option to show worst prediction in dataset

--- predict.py
+++ predict.py
@@ -11,10 +11,11 @@ import utils
 compressed = True
 dataset = 'data/test.tar'
 index = 0 # plot the prediction for the following sample in the set
-model_name = 'run5v2_naKd3sF2mK'
+model_name = '64_r1_naKd4sF2mK'
 model_version = 'e95'
-compute_prediction_error = True
+compute_prediction_error = False
 use_terrain_mask = True
+plot_worst_prediction = False
 # --------------------------------------------------------------------------
 
 # check if gpu available
@@ -43,17 +44,26 @@ loss_fn = torch.nn.MSELoss()
 
 #Compute the average loss
 with torch.no_grad():
+    worst_index = -1
+    maxloss = -200000.0 #arbitrary large negative number, the loss should always be higher than that number
+
     if compute_prediction_error:
         loss = 0.0
         loss_ux = 0.0
         loss_uy = 0.0
         loss_uz = 0.0
         loss_nut = 0.0
-        for data in testloader:
+        for i, data in enumerate(testloader):
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
             outputs = net(inputs)
-            loss += loss_fn(outputs, labels)
+
+            dloss = loss_fn(outputs, labels)
+            if dloss > maxloss:
+                maxloss = dloss
+                worst_index = i
+
+            loss += dloss
             if params.model['d3']:
                 loss_ux += loss_fn(outputs[:,0,:,:,:], labels[:,0,:,:,:])
                 loss_uy += loss_fn(outputs[:,1,:,:,:], labels[:,1,:,:,:])
@@ -75,10 +85,17 @@ with torch.no_grad():
             print('INFO: Average loss on test set for turbulence: %s' % (loss_nut.item()/len(testset)))
 
     # plot prediction
-    input, label = testset[index]
+    if plot_worst_prediction and (worst_index != -1):
+        print('Plotting sample with the highest loss: {}'.format(maxloss))
+        print(worst_index)
+        input, label = testset[worst_index]
+    else:
+        input, label = testset[index]
+
     input, label = input.to(device), label.to(device)
     output = net(input.unsqueeze(0))
     input = input.squeeze()
     output = output.squeeze()
+    print('Plotting sample with loss: {}'.format(loss_fn(output, label)))
 
     utils.plot_prediction(output, label)
