1e3eb81d5ae93733152fcd65eba0e72a3a92ace3
Nicholas Lawrance <lawrancn@eu-login-19-ng.euler.ethz.ch>
2018-11-14 16:23:09 +0100

    Update make_stl for phstl directory argument

--- data_generation/openfoam_batch/make_stl.sh
+++ data_generation/openfoam_batch/make_stl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# ./make_stl.sh DIRECTORY_WITH_TIFS DIRECTORY_FOR_STLS
-for file in $1/*.tif; do 
+# ./make_stl.sh PHSTL_DIR DIRECTORY_WITH_TIFS DIRECTORY_FOR_STLS
+for file in $2/*.tif; do 
 	base_file=$( basename $file .tif); 
-	python phstl/phstl.py "${file}" "${2}/${base_file}.stl"
+	python "${1}/phstl.py" "${file}" "${3}/${base_file}.stl"
 done
