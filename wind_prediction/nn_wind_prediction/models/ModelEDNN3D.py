e9a8f73f6fc651d6fd963d7e309bcef92c2b19c4
acfloria <florian.achermann@mavt.ethz.ch>
2018-11-15 14:51:27 +0100

    Fix bug in stacked network

--- wind_prediction/nn_wind_prediction/models/ModelEDNN3D.py
+++ wind_prediction/nn_wind_prediction/models/ModelEDNN3D.py
@@ -1,3 +1,4 @@
+import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -42,109 +43,113 @@ class ModelEDNN3D(nn.Module):
         try:
             self.__use_terrain_mask = kwargs['use_terrain_mask']
         except KeyError:
-            self.__use_terrain_mask = __default_use_terrain_mask
+            self.__use_terrain_mask = self.__default_use_terrain_mask
             if verbose:
-                print('EDNN3D: use_terrain_mask not present in kwargs, using default value:', __default_use_terrain_mask)
+                print('EDNN3D: use_terrain_mask not present in kwargs, using default value:', self.__default_use_terrain_mask)
 
         try:
             self.__n_downsample_layers = kwargs['n_downsample_layers']
         except KeyError:
-            self.__n_downsample_layers = __default_n_downsample_layers
+            self.__n_downsample_layers = self.__default_n_downsample_layers
             if verbose:
-                print('EDNN3D: n_downsample_layers not present in kwargs, using default value:', __default_n_downsample_layers)
+                print('EDNN3D: n_downsample_layers not present in kwargs, using default value:', self.__default_n_downsample_layers)
 
         try:
             self.__use_mapping_layer = kwargs['use_mapping_layer']
         except KeyError:
-            self.__use_mapping_layer = __default_use_mapping_layer
+            self.__use_mapping_layer = self.__default_use_mapping_layer
             if verbose:
-                print('EDNN3D: use_mapping_layer not present in kwargs, using default value:', __default_use_mapping_layer)
+                print('EDNN3D: use_mapping_layer not present in kwargs, using default value:', self.__default_use_mapping_layer)
 
         try:
             self.__use_fc_layers = kwargs['use_fc_layers']
         except KeyError:
-            self.__use_fc_layers = __default_use_fc_layers
+            self.__use_fc_layers = self.__default_use_fc_layers
             if verbose:
-                print('EDNN3D: use_fc_layers not present in kwargs, using default value:', __default_use_fc_layers)
+                print('EDNN3D: use_fc_layers not present in kwargs, using default value:', self.__default_use_fc_layers)
 
         try:
             self.__fc_scaling = kwargs['fc_scaling']
         except KeyError:
-            self.__fc_scaling = __default_fc_scaling
+            self.__fc_scaling = self.__default_fc_scaling
             if verbose:
-                print('EDNN3D: fc_scaling not present in kwargs, using default value:', __default_fc_scaling)
+                print('EDNN3D: fc_scaling not present in kwargs, using default value:', self.__default_fc_scaling)
 
         try:
             self.__potential_flow = kwargs['potential_flow']
         except KeyError:
-            self.__potential_flow = __default_potential_flow
+            self.__potential_flow = self.__default_potential_flow
             if verbose:
-                print('EDNN3D: potential_flow not present in kwargs, using default value:', __default_potential_flow)
+                print('EDNN3D: potential_flow not present in kwargs, using default value:', self.__default_potential_flow)
 
         try:
             self.__n_x = kwargs['n_x']
         except KeyError:
-            self.__n_x = __default_n_x
+            self.__n_x = self.__default_n_x
             if verbose:
-                print('EDNN3D: n_x not present in kwargs, using default value:', __default_n_x)
+                print('EDNN3D: n_x not present in kwargs, using default value:', self.__default_n_x)
 
         try:
             self.__n_y = kwargs['n_y']
         except KeyError:
-            self.__n_y = __default_n_y
+            self.__n_y = self.__default_n_y
             if verbose:
-                print('EDNN3D: n_y not present in kwargs, using default value:', __default_n_y)
+                print('EDNN3D: n_y not present in kwargs, using default value:', self.__default_n_y)
 
         try:
             self.__n_z = kwargs['n_z']
         except KeyError:
-            self.__n_z = __default_n_z
+            self.__n_z = self.__default_n_z
             if verbose:
-                print('EDNN3D: n_z not present in kwargs, using default value:', __default_n_z)
+                print('EDNN3D: n_z not present in kwargs, using default value:', self.__default_n_z)
 
         try:
             self.__interpolation_mode = kwargs['interpolation_mode']
         except KeyError:
-            self.__interpolation_mode = __default_interpolation_mode
+            self.__interpolation_mode = self.__default_interpolation_mode
             if verbose:
-                print('EDNN3D: interpolation_mode not present in kwargs, using default value:', __default_interpolation_mode)
+                print('EDNN3D: interpolation_mode not present in kwargs, using default value:', self.__default_interpolation_mode)
 
         try:
             self.__skipping = kwargs['skipping']
         except KeyError:
-            self.__skipping = __default_skipping
+            self.__skipping = self.__default_skipping
             if verbose:
-                print('EDNN3D: skipping not present in kwargs, using default value:', __default_skipping)
+                print('EDNN3D: skipping not present in kwargs, using default value:', self.__default_skipping)
 
         try:
             self.__align_corners = kwargs['align_corners']
             if self.__align_corners == False:
                 self.__align_corners = None
         except KeyError:
-            self.__align_corners = __default_align_corners
+            self.__align_corners = self.__default_align_corners
             if verbose:
-                print('EDNN3D: align_corners not present in kwargs, using default value:', __default_align_corners)
+                print('EDNN3D: align_corners not present in kwargs, using default value:', self.__default_align_corners)
 
         try:
             self.__pooling_method = kwargs['pooling_method']
         except KeyError:
-            self.__pooling_method = __default_pooling_method
+            self.__pooling_method = self.__default_pooling_method
             if verbose:
-                print('EDNN3D: pooling_method not present in kwargs, using default value:', __default_pooling_method)
+                print('EDNN3D: pooling_method not present in kwargs, using default value:', self.__default_pooling_method)
 
         try:
             self.__use_grid_size = kwargs['use_grid_size']
         except KeyError:
-            self.__use_grid_size = __default_use_grid_size
+            self.__use_grid_size = self.__default_use_grid_size
             if verbose:
-                print('EDNN3D: use_grid_size not present in kwargs, using default value:', __default_use_grid_size)
+                print('EDNN3D: use_grid_size not present in kwargs, using default value:', self.__default_use_grid_size)
 
         try:
             self.__use_turbulence = kwargs['use_turbulence']
         except KeyError:
-            self.__use_turbulence = __default_use_turbulence
+            self.__use_turbulence = self.__default_use_turbulence
             if verbose:
-                print('EDNN3D: use_turbulence not present in kwargs, using default value:', __default_use_turbulence)
+                print('EDNN3D: use_turbulence not present in kwargs, using default value:', self.__default_use_turbulence)
+
+        if self.__n_downsample_layers <= 0:
+            print('EDNN3D: Error, n_downsample_layers must be larger than 0')
+            sys.exit()
 
         # construct the number of input and output channels based on the parameters
         self.__num_inputs = 4 # (terrain, u_x_in, u_y_in, u_z_in)
--- wind_prediction/nn_wind_prediction/models/ModelStacked.py
+++ wind_prediction/nn_wind_prediction/models/ModelStacked.py
@@ -4,6 +4,10 @@ import torch
 import torch.nn as nn
 
 class ModelStacked(nn.Module):
+    __default_pass_full_output = False
+    __default_submodel_terrain_mask = False
+    __default_use_terrain_mask = True
+
     def __init__(self, **kwargs):
         super(ModelStacked, self).__init__()
 
@@ -28,12 +32,49 @@ class ModelStacked(nn.Module):
             print('ModelStacked ERROR: The module has no attribute:', kwargs['submodel_type'])
             sys.exit()
 
+        try:
+            self.__pass_full_output = kwargs['pass_full_output']
+        except KeyError:
+            self.__pass_full_output = self.__default_pass_full_output
+            if verbose:
+                print('ModelStacked WARNING: pass_full_output not present in kwargs, using default value:', self.__default_pass_full_output)
+
+        try:
+            self.__submodel_terrain_mask = kwargs['submodel_terrain_mask']
+        except KeyError:
+            self.__submodel_terrain_mask = self.__default_submodel_terrain_mask
+            if verbose:
+                print('ModelStacked WARNING: submodel_terrain_mask not present in kwargs, using default value:', self.__default_submodel_terrain_mask)
+
+        try:
+            self.__submodel_terrain_mask = kwargs['submodel_terrain_mask']
+        except KeyError:
+            self.__submodel_terrain_mask = self.__default_submodel_terrain_mask
+            if verbose:
+                print('ModelStacked WARNING: submodel_terrain_mask not present in kwargs, using default value:', self.__default_submodel_terrain_mask)
+
+        try:
+            self.__use_terrain_mask = kwargs['use_terrain_mask']
+        except KeyError:
+            self.__use_terrain_mask = self.__default_use_terrain_mask
+            if verbose:
+                print('ModelStacked WARNING: use_terrain_mask not present in kwargs, using default value:', self.__default_use_terrain_mask)
+
+        # generate the stacked models based on the input params
         self.__models = nn.ModuleList()
+        kwargs['use_terrain_mask'] = self.__submodel_terrain_mask
         self.__models += [Model(**kwargs)]
-        kwargs['force_num_inputs'] = self.__models[0].num_outputs()
+
+        if self.__pass_full_output:
+            kwargs['force_num_inputs'] = self.__models[0].num_outputs() + self.__models[0].num_inputs()
+        else:
+            kwargs['force_num_inputs'] = self.__models[0].num_outputs() + 1
+
         kwargs['force_num_outputs'] = self.__models[0].num_outputs()
 
         for i in range(1, N):
+            if i == (N -1):
+                kwargs['use_terrain_mask'] = self.__use_terrain_mask
             self.__models += [Model(**kwargs)]
 
     def init_params(self):
@@ -61,6 +102,17 @@ class ModelStacked(nn.Module):
         self.__models[N].unfreeze_model()
 
     def forward(self, x):
+        input = x.clone()
+        first_iter = True
         for model in self.__models:
-            x = model(x)
+            if first_iter:
+                x = model(x)
+                first_iter = False
+            else:
+                if self.__pass_full_output:
+                    x = model(torch.cat((x, input),1))
+                else:
+                    # only pass the terrain information
+                    x = model(torch.cat((x, input[:,0,:].unsqueeze(1)),1))
+
         return x
