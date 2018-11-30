a279f633ffcb3e995584f73dec6c79c035592f1b
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2018-12-01 00:29:13 +0100

    Skip the quaternion interpolation (slerp), add wind plots

--- wind_analysis/plot_basic_wind.py
+++ wind_analysis/plot_basic_wind.py
@@ -3,6 +3,10 @@ from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import utm
 from pyulog.core import ULog
+import os
+
+savefig=False
+logfile = 'logs/13_02_02.ulg'
 
 def rpy(roll, pitch, yaw):
     ra = np.array([[np.cos(roll), -np.sin(roll), 0],
@@ -20,6 +24,15 @@ def quaternion_rotation_matrix(q):
                   [2*(qi*qk - qj*qr), 2*(qj*qk + qi*qr), 1-2*(qi**2+qj**2)]])
     return R
 
+def get_colors(uu, vv, ww, cmap=plt.cm.hsv):
+    # Color by azimuthal angle
+    c = np.sqrt(uu**2 + vv**2 + ww**2)
+    # Flatten and normalize
+    c = (c.ravel() - c.min()) / c.ptp()
+    # Repeat for each body line and two head lines
+    c = np.concatenate((c, np.repeat(c, 2)))
+    # Colormap
+    return cmap(c)
 
 def slerp(v0, v1, t_array):
     # >>> slerp([1,0,0,0],[0,0,0,1],np.arange(0,1,0.001))
@@ -46,7 +59,6 @@ def slerp(v0, v1, t_array):
 def get_quat(data, index):
     return(np.array([data['q[0]'][index], data['q[1]'][index], data['q[2]'][index], data['q[3]'][index]]))
 
-logfile = 'logs/13_02_02.ulg'
 
 log_data = ULog(logfile).data_list
 
@@ -83,15 +95,6 @@ we_east = np.interp(skip_time, data_dict['wind_estimate']['timestamp'], data_dic
 we_north = np.interp(skip_time, data_dict['wind_estimate']['timestamp'], data_dict['wind_estimate']['windspeed_north'])
 we_up = np.zeros(we_north.shape)
 
-# Color by azimuthal angle
-c = np.sqrt(we_east**2 + we_north**2)
-# Flatten and normalize
-c = (c.ravel() - c.min()) / c.ptp()
-# Repeat for each body line and two head lines
-c = np.concatenate((c, np.repeat(c, 2)))
-# Colormap
-c = plt.cm.hsv(c)
-
 # Raw wind estimates from alpha, beta !THIS IS UNCORRECTED!
 alpha_skip = np.interp(skip_time, data_dict['sensor_hall']['timestamp'], data_dict['sensor_hall']['mag_T'])*np.pi/180.0
 beta_skip = np.interp(skip_time, data_dict['sensor_hall_01']['timestamp'], data_dict['sensor_hall_01']['mag_T'])*np.pi/180.0
@@ -104,20 +107,25 @@ w = V_skip*np.sin(alpha_skip)*np.cos(beta_skip)
 
 # I NEED INTERPOLATED QUATERNIONS!!
 # Get interpolated time steps
-wn, we, wd = [], [], []
+n_skip = len(skip_time)
+wn, we, wd = np.zeros(n_skip), np.zeros(n_skip), np.zeros(n_skip)
 for i, st in enumerate(skip_time):
     tdex = np.sum(st > data_dict['vehicle_attitude']['timestamp'])
-    if tdex < len(data_dict['vehicle_attitude']['timestamp']):
-        tu, tl = data_dict['vehicle_attitude']['timestamp'][tdex+1], data_dict['vehicle_attitude']['timestamp'][tdex]
-        t_scale = (st - tl) / (tu - tl)
-        qq = slerp(get_quat(data_dict['vehicle_attitude'], tdex), get_quat(data_dict['vehicle_attitude'], tdex+1), [t_scale])
-    else:
-        qq = get_quat(data_dict['vehicle_attitude'], tdex)
+    # if tdex < len(data_dict['vehicle_attitude']['timestamp']):
+    #     tu, tl = data_dict['vehicle_attitude']['timestamp'][tdex+1], data_dict['vehicle_attitude']['timestamp'][tdex]
+    #     t_scale = (st - tl) / (tu - tl)
+    #     qq = slerp(get_quat(data_dict['vehicle_attitude'], tdex), get_quat(data_dict['vehicle_attitude'], tdex+1), [t_scale])
+    # else:
+
+    qq = get_quat(data_dict['vehicle_attitude'], tdex)
     R = quaternion_rotation_matrix(qq.flat)
     Vg = np.matmul(R, np.array([[u[i]], [v[i]], [w[i]]]))
-    wn.append(Vg[0] - skip_vel_n[i])
-    we.append(Vg[1] - skip_vel_e[i])
-    wd.append(Vg[2] - skip_vel_d[i])
+    wn[i] = (Vg[0] - skip_vel_n[i])
+    we[i] = (Vg[1] - skip_vel_e[i])
+    wd[i] = (Vg[2] - skip_vel_d[i])
+
+# For some reason they are negative... check signs
+wn, we = -wn, -we
 
 # Plot the wind vector estimates
 fig = plt.figure()
@@ -127,8 +135,24 @@ ax.set_xlabel('Easting (m)')
 ax.set_ylabel('Northing (m)')
 ax.set_zlabel('Altitude (m)')
 
-ax.quiver(x[::skip_amount], y[::skip_amount], alt[::skip_amount], we_east, we_north, we_up, colors=c)
-# ax.quiver(x[::skip_amount], y[::skip_amount], alt[::skip_amount], np.array(we), np.array(wn), -np.array(wd), colors=c)
+xx, yy, zz = x[::skip_amount]-x[0], y[::skip_amount]-y[0], alt[::skip_amount]-alt[0]
+ax.quiver(xx, yy, zz, we_east, we_north, we_up, colors=get_colors(we_east, we_north, we_up))
+ax.quiver(xx, yy, zz, we, wn, -wd, colors=get_colors(we, wn, wd, plt.cm.viridis))
 
+f2, a2 = plt.subplots(3, 1)
+h_est = a2[0].plot(skip_time, we_east)
+h_vanes = a2[0].plot(skip_time, we)
+a2[0].legend(['Pixhawk estimate', 'Vane estimate (raw)'])
+a2[0].set_ylabel('$V_E$')
+a2[1].plot(skip_time, we_north)
+a2[1].plot(skip_time, wn)
+a2[1].set_ylabel('$V_N$')
+a2[2].plot(skip_time, we_up)
+a2[2].plot(skip_time, -wd)
+a2[2].set_ylabel('$V_D$')
 
+if savefig:
+    bn = os.path.basename(logfile)
+    fig.savefig('fig/{0}_wind3d.png'.format(bn), bbox_inches='tight')
+    f2.savefig('fig/{0}_wind.png'.format(bn), bbox_inches='tight')
 plt.show()
