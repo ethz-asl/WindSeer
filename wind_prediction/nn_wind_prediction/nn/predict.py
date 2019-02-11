Fix error in prediction error calculation with scaled and normalized terrain

wind_prediction/nn_wind_prediction/nn/predict.py
@@ -189,7 +189,12 @@ def dataset_prediction_error(net, device, params, loss_fn, loader_testset):
                 raise ValueError
 
             # compute the prediction errors and extract the data
-            error_stats = utils.prediction_error.compute_prediction_error(labels, outputs, inputs[0,0], predict_uncertainty, device, params.data['use_turbulence'])
+            error_stats = utils.prediction_error.compute_prediction_error(labels,
+                                                                          outputs,
+                                                                          inputs[0,0] * params.data['terrain_scaling'],
+                                                                          predict_uncertainty, device,
+                                                                          params.data['use_turbulence'],
+                                                                          params.data['normalize_terrain'])
             for key in error_stats.keys():
                 if not np.isnan(error_stats[key]):
                     prediction_errors[key].append(error_stats[key])

wind_prediction/nn_wind_prediction/utils/prediction_error.py
@@ -2,9 +2,9 @@ import numpy as np
 from scipy import ndimage
 import torch
 
-threshold_low = 7.0 # this corresponds to roughly 80 m
+threshold_low = 7.0 # this corresponds to roughly 80 m for the unscaled terrain
 
-def compute_prediction_error(label, prediction, terrain, uncertainty_predicted, device, turbulence):
+def compute_prediction_error(label, prediction, terrain, uncertainty_predicted, device, turbulence, normalized_terrain):
     if uncertainty_predicted:
         abs_error = (label - prediction[:label.shape[0]]).abs()
     else:
@@ -13,8 +13,10 @@ def compute_prediction_error(label, prediction, terrain, uncertainty_predicted, 
     d3 = (len(list(abs_error.size())) > 3)
 
     # convert the boolean terrain into a distance field if required
-    if (terrain.max().item() == 1.0) and (torch.mul(torch.gt(terrain, torch.zeros_like(terrain)), torch.gt(torch.ones_like(terrain), terrain)).sum() == 0):
-        terrain = torch.from_numpy(ndimage.distance_transform_edt(terrain.cpu().numpy()).astype(np.float32)).to(device)
+    if ((terrain.max().item() == 1.0) and
+            (torch.mul(torch.gt(terrain, torch.zeros_like(terrain)), torch.gt(torch.ones_like(terrain), terrain)).sum() == 0) or
+            normalized_terrain):
+        terrain = torch.from_numpy(ndimage.distance_transform_edt(terrain.sign().cpu().numpy()).astype(np.float32)).to(device)
 
     # create the masks
     mask_low = torch.mul((terrain <= threshold_low), (terrain > 0.0))