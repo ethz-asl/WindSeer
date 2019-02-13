6213e7f96a6c69b878b57e646b22fd936da06211
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2019-02-13 13:44:10 +0100

    Include grid offset file chenyx06 and update install script

--- wind_analysis/README.md
+++ wind_analysis/README.md
@@ -2,7 +2,10 @@
 
 ## Installation
 
-1. This code requires installation of the GDAL (v 2.3.2) library for working with geospatial data (geotiffs)
+1. Install proj
+    `sudo apt-get install proj-bin`
+
+2. This code requires installation of the GDAL (v 2.3.2) library for working with geospatial data (geotiffs)
     ~~~
     wget http://download.osgeo.org/gdal/2.3.2/gdal-2.3.2.tar.gz
     tar -xzf gdal-2.3.2.tar.gz
@@ -13,16 +16,16 @@
     gdalinfo --version
     ~~~
 
-2. Install pygdal library
+3. Install pygdal library
     `pip install pygdal==2.3.2.4`
 
-3. Other dependencies
+4. Other dependencies
     `pip install numpy scipy pyproj`
 
-4. Need to manually add the CH1903 definitions to pyproj. You can try to use this simple script.
+5. Need to manually add the CH1903 definitions to pyproj. You can try to use this simple script.
     ~~~
     cd intel_wind/wind_analysis/proj_definitions
     python install_ch_defs.py
     ~~~
-    If you have permission errors, you basically need to install the `CH` file in `pyproj_definitions` to the `data` subdirectory of the pyproj module. The script above should have returned an error specifying where the file was supposed to go. You can copy the file there manually with the required permissions.
+    If you have permission errors, you basically need to install the `CH` and `chenyx06etrs.gsb` files in `pyproj_definitions` to the `data` subdirectory of the pyproj module. The script above should have returned an error specifying where the file was supposed to go. You can copy the file there manually with the required permissions.
     
--- wind_analysis/proj_definitions/install_ch_defs.py
+++ wind_analysis/proj_definitions/install_ch_defs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import os
-import inspect
 from shutil import copyfile
 
 try:
@@ -11,25 +10,36 @@ except ImportError:
     print("     pip install pyproj")
     exit()
 
-# Get the pyproj location
-pyproj_dir = os.path.dirname(inspect.getfile(pyproj))
-ch_target = os.path.join(pyproj_dir, 'data', 'CH')
 
-# Copy the CH file into the pyproj data directory
-if os.path.isfile(ch_target):
-    print('{0} file already present!'.format(ch_target))
-else:
-    print('Attempting to copy CH to {0}'.format(ch_target))
-    try:
-        copyfile('CH', ch_target)
-    except:
-        print('Copy failed. Please copy the CH file manually to the specified location.')
-        raise
+def copy_with_check(src, dst):
+    if os.path.isfile(dst):
+        print('{0} file already exists, not copying.'.format(dst))
+    else:
+        print('Attempting to copy {0} to {1}'.format(src, dst))
+        try:
+            copyfile(src, dst)
+            print('Copied {0} to {1}'.format(src, dst))
+        except:
+            print('Copy failed. Please copy the file manually to the specified location.')
+            raise
+
+
+# Copy files to pyproj data directory
+for file in ['CH', 'chenyx06etrs.gsb']:
+    destination = os.path.join(pyproj.pyproj_datadir, file)
+    copy_with_check(file, destination)
 
 # Check if it worked:
 try:
-    proj_output = pyproj.Proj(init="CH:1903_LV03")
+    proj_ch = pyproj.Proj(init="CH:1903_LV03")
     print('Loading CH:1903_LV03 projection succeeded!')
+    proj_wgs84 = pyproj.Proj(proj='latlong', datum='WGS84')
+    print('Loading WGS84 projection succeeded!')
+    [e], [n], [alt] = pyproj.transform(proj_wgs84, proj_ch, [7.438632451], [46.951082814], [248.913])
+    if abs(e - 6e5) < 1.0 and abs(n - 2e5) < 1.0:
+        print('Successful conversion from WGS84 -> LV03')
+    else:
+        print('Conversion failed. e = {0:0.5f}, n = {0:0.5f}')
 except:
     print('Loading projection failed!')
     raise
