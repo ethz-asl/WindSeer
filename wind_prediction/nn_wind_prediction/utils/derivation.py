37d03ae0860d910a9854318cd066b252b4ad438a
Gregory Duthe <gregory.duthe@hotmail.com>
2019-03-11 10:52:33 +0100

    Fixed bug in curl function

--- wind_prediction/nn_wind_prediction/utils/derivation.py
+++ wind_prediction/nn_wind_prediction/utils/derivation.py
@@ -6,7 +6,7 @@ def derive(input_tensor, deriv_axis, ds=1):
     This function computes the derivative of a scalar field, with regard to a specified axis direction
 
     Input params:
-        input_tensor: 4D tensor (samples x Depth x Height x Width)
+        input_tensor: 4D tensor (samples x Z x Y x X)
         deriv_axis: axis along which to compute the derivative
         ds: grid size of deriv_axis, default is unit.
 
@@ -15,13 +15,14 @@ def derive(input_tensor, deriv_axis, ds=1):
                 Implementation of first order centered finite differences. 0 padding for first and last element
     '''
     # Permute axes depending on deriv_axs
-    if deriv_axis == 1:
+    if deriv_axis == 1:  # Z
         input_tensor = input_tensor.permute(0, 3, 2, 1)
 
-    elif deriv_axis == 2:
+    elif deriv_axis == 2:  # Y
         input_tensor = input_tensor.permute(0, 1, 3, 2)
 
-    elif deriv_axis == 3:
+
+    elif deriv_axis == 3:  # X
         input_tensor = input_tensor.permute(0, 1, 2, 3)
 
     else:
@@ -64,19 +65,19 @@ def curl(input_tensor, ds=1):
     This function computes the curl of a vector field, with regard to a specified grid size
 
     Input params:
-        input_tensor: 5D tensor (samples, input_field[phix_in, phiy_in, phiz_in]), X, Y, Z)
+        input_tensor: 5D tensor (samples, input_field[phix_in, phiy_in, phiz_in]), Z, Y, X)
         ds: grid size, default is unit.
 
     Output:
         curled_tensor: 5D tensor (samples, curled_field[u, v, w]), X, Y, Z)
     '''
     phix_y = derive(input_tensor[:, 0, :, :, :], 2, ds)
-    phix_z = derive(input_tensor[:, 0, :, :, :], 3, ds)
+    phix_z = derive(input_tensor[:, 0, :, :, :], 1, ds)
 
-    phiy_x = derive(input_tensor[:, 1, :, :, :], 1, ds)
-    phiy_z = derive(input_tensor[:, 1, :, :, :], 3, ds)
+    phiy_x = derive(input_tensor[:, 1, :, :, :], 3, ds)
+    phiy_z = derive(input_tensor[:, 1, :, :, :], 1, ds)
 
-    phiz_x = derive(input_tensor[:, 2, :, :, :], 1, ds)
+    phiz_x = derive(input_tensor[:, 2, :, :, :], 3, ds)
     phiz_y = derive(input_tensor[:, 2, :, :, :], 2, ds)
 
     # curled vector field components
