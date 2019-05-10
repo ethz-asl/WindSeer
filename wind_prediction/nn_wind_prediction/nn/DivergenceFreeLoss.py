Rebased on master and fixed merging errors. Data in terrain is now excluded during loss computation.

wind_prediction/nn_wind_prediction/nn/DivergenceFreeLoss.py
@@ -2,6 +2,7 @@ from torch.nn import Module
 import torch
 import torch.nn.functional as f
 import nn_wind_prediction.utils as utils
+from nn_wind_prediction.utils import remove_terrain_data
 
 class DivergenceFreeLoss(Module):
     '''
@@ -13,16 +14,16 @@ class DivergenceFreeLoss(Module):
         grid_size: list containing the grid spacing in directions X, Y and Z of the dataset. [m]
         div_scaling: scaling factor used to balance the two components of the loss.
     '''
-    __default_loss_method_type = 'MSE'
+    __default_loss_method = 'MSE'
     __default_grid_size = [1, 1, 1]
     __default_scaling_factor = 500 #found to work well (trial and error)
 
     def __init__(self, **kwargs):
         super(DivergenceFreeLoss, self).__init__()
         try:
-            loss_method_type = kwargs['loss_method_type']
+            self.__loss_method = kwargs['loss_method']
         except KeyError:
-            loss_method_type = self.__default_loss_method_type
+            self.__loss_method = self.__default_loss_method
             print('DivergenceFreeLoss: loss_method not present in kwargs, using default value:', self.__default_loss_method)
 
         try:
@@ -38,16 +39,16 @@ class DivergenceFreeLoss(Module):
             print('DivergenceFreeLoss: scaling_factor not present in kwargs, using default value:',
                   self.__default_scaling_factor)
 
-        if (loss_method_type == 'MSE'):
+        if (self.__loss_method == 'MSE'):
             self.__loss = torch.nn.MSELoss()
-        elif (loss_method_type == 'L1'):
+        elif (self.__loss_method == 'L1'):
             self.__loss = torch.nn.L1Loss()
         else:
-            raise ValueError('Only L1 and MSE loss_type supported')
+            raise ValueError('Unknown loss type: ', self.__loss_method)
 
-    def forward(self, net_output, target):
+    def forward(self, net_output, target, terrain = None):
         if (net_output.shape != target.shape):
-            raise ValueError('Prediction and target do not have the same shape')
+            raise ValueError('Prediction and target do not have the same shape, pred:{}, target:{}'.format(net_output.shape,target.shape))
 
         if (len(net_output.shape) != 5):
             if (len(net_output.shape) == 4) and net_output.shape[0]==3: # corresponds to a single sample
@@ -55,10 +56,16 @@ class DivergenceFreeLoss(Module):
             else:
              raise ValueError('The loss is only defined for 5D data')
 
-        return self.compute_loss(net_output, target)
+        return self.compute_loss(net_output, target, terrain)
 
-    def compute_loss(self, net_output, target):
-        diverged_output = utils.divergence(net_output, self.__grid_size)
+    def compute_loss(self, net_output, target, terrain):
+        diverged_output = utils.divergence(net_output, self.__grid_size).unsqueeze(1)
+
+        # remove data in terrain
+        if terrain is not None:
+            target = remove_terrain_data(target, terrain)
+            net_output = remove_terrain_data(net_output, terrain)
+            diverged_output = remove_terrain_data(diverged_output, terrain)
 
         loss = self.__loss(target, net_output)
         loss += self.__div_scaling *self.__loss(diverged_output, torch.zeros_like(diverged_output))

wind_prediction/nn_wind_prediction/nn/GaussianLogLikelihoodLoss.py
@@ -1,6 +1,6 @@
 import torch
 from torch.nn import Module
-
+from nn_wind_prediction.utils import remove_terrain_data
 class GaussianLogLikelihoodLoss(Module):
     '''
     Gaussian Log Likelihood Loss according to https://arxiv.org/pdf/1705.07115.pdf
@@ -17,19 +17,23 @@ class GaussianLogLikelihoodLoss(Module):
             self.__eps = self.__default_eps
             print('GaussianLogLikelihoodLoss: uncertainty_loss_eps not present in kwargs, using default value:', self.__eps)
 
-    def forward(self, output, label):
+    def forward(self, output, label, terrain=None):
         num_channels = output.shape[1]
 
         if (output.shape[1] != 2 * label.shape[1]) and (num_channels % 2 != 0):
             raise ValueError('The output has to have twice the number of channels of the labels')
 
-        return self.compute_loss(output[:,:int(num_channels/2),:], output[:,int(num_channels/2):,:], label)
+        return self.compute_loss(output[:,:int(num_channels/2),:], output[:,int(num_channels/2):,:], label, terrain)
 
-    def compute_loss(self, mean, log_variance, label):
+    def compute_loss(self, mean, log_variance, label, terrain):
         if (mean.shape[1] != log_variance.shape[1]):
             raise ValueError('The variance and the mean need to have the same number of channels')
 
         mean_error =  mean - label
+
+        # remove data in terrain
+        if terrain is not None:
+            mean_error = remove_terrain_data(mean_error, terrain)
 
         loss = log_variance + (mean_error * mean_error) / log_variance.exp().clamp(self.__eps)
 

wind_prediction/nn_wind_prediction/nn/VelocityGradientLoss.py
@@ -1,6 +1,6 @@
 from torch.nn import Module
 import torch
-import torch.nn.functional as f
+from nn_wind_prediction.utils import remove_terrain_data
 import nn_wind_prediction.utils as utils
 
 class VelocityGradientLoss(Module):
@@ -14,16 +14,16 @@ class VelocityGradientLoss(Module):
         grid_size: list containing the grid spacing in directions X, Y and Z of the dataset. [m]
         grad_scaling: scaling factor used to balance the two components of the loss.
     '''
-    __default_loss_method_type = 'MSE'
+    __default_loss_method = 'MSE'
     __default_grid_size = [1, 1, 1]
     __default_scaling_factor = 1200 #found to work well (trial and error)
 
     def __init__(self, **kwargs):
         super(VelocityGradientLoss, self).__init__()
         try:
-            loss_method_type = kwargs['loss_method_type']
+            self.__loss_method = kwargs['loss_method']
         except KeyError:
-            loss_method_type = self.__default_loss_method_type
+            self.__loss_method = self.__default_loss_method
             print('DivergenceFreeLoss: loss_method not present in kwargs, using default value:', self.__default_loss_method)
 
         try:
@@ -39,14 +39,14 @@ class VelocityGradientLoss(Module):
             print('DivergenceFreeLoss: scaling_factor not present in kwargs, using default value:',
                   self.__default_scaling_factor)
 
-        if (loss_method_type == 'MSE'):
+        if (self.__loss_method == 'MSE'):
             self.__loss = torch.nn.MSELoss()
-        elif (loss_method_type == 'L1'):
+        elif (self.__loss_method == 'L1'):
             self.__loss = torch.nn.L1Loss()
         else:
-            raise ValueError('Only L1 and MSE loss_type supported')
+            raise ValueError('Unknown loss type: ', self.__loss_method)
 
-    def forward(self, net_output, target):
+    def forward(self, net_output, target, terrain=None):
         if (net_output.shape != target.shape):
             raise ValueError('Prediction and target do not have the same shape')
 
@@ -57,11 +57,19 @@ class VelocityGradientLoss(Module):
             else:
                 raise ValueError('The loss is only defined for 5D data')
 
-        return self.compute_loss(net_output, target)
+        return self.compute_loss(net_output, target, terrain)
 
-    def compute_loss(self, net_output, target):
+    def compute_loss(self, net_output, target, terrain):
+        target_grad = utils.gradient(target,self.__grid_size)
+        net_output_grad = utils.gradient(net_output,self.__grid_size)
+
+        # remove data in terrain
+        if terrain is not None:
+            target = remove_terrain_data(target, terrain)
+            net_output = remove_terrain_data(net_output, terrain)
+            target_grad = remove_terrain_data(target_grad, terrain)
+            net_output_grad = remove_terrain_data(net_output_grad, terrain)
 
         loss = self.__loss(target, net_output)
-        loss += self.__grad_scaling*self.__loss(utils.gradient(target,self.__grid_size),
-                                                  utils.gradient(net_output,self.__grid_size))
+        loss += self.__grad_scaling*self.__loss(net_output_grad, target_grad)
         return loss

wind_prediction/train.py
@@ -8,15 +8,13 @@ import nn_wind_prediction.models as models
 import nn_wind_prediction.nn as nn_custom
 import nn_wind_prediction.utils as utils
 import os
-import random
-import string
+import re
 import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import StepLR
-import numpy as np
 
 now_time = time.strftime("%Y_%m_%d-%H_%M")
 
@@ -82,18 +80,31 @@ NetworkType = getattr(models, run_params.model['model_type'])
 # get grid size
 grid_size = data.get_grid_size(trainset_name)
 run_params.model_kwargs()['grid_size'] = grid_size
-run_params.loss_function_kwargs()['grid_size'] = grid_size
+run_params.loss_kwargs()['grid_size'] = grid_size
 
 
 net = NetworkType(**run_params.model_kwargs())
 
+warm_start_epoch = 0
 if (run_params.run['warm_start']):
     try:
         net.load_state_dict(torch.load(os.path.join(model_dir, 'pretrained.model'), map_location=lambda storage, loc: storage))
     except:
-        print('Warning: Failed to load the model parameter, initializing parameter.')
-        if run_params.run['custom_init']:
-            net.init_params()
+        try:
+            print('Warm start warning: Failed to load pretrained.model. Using models from latest saved epoch.')
+
+            saved_model_epochs = []
+            for filename in os.listdir(model_dir):
+                if filename.startswith('e') and filename.endswith('model'):
+                    saved_model_epochs += [int(re.search(r'\d+', filename).group())]
+            warm_start_epoch = max(saved_model_epochs)
+
+            net.load_state_dict(torch.load(os.path.join(model_dir, 'e{}.model'.format(warm_start_epoch)),
+                                           map_location=lambda storage, loc: storage))
+        except:
+            print('Warm start warning: Failed to load the model parameter, initializing parameter.')
+            if run_params.run['custom_init']:
+                net.init_params()
 else:
     if run_params.run['custom_init']:
         net.init_params()
@@ -108,19 +119,21 @@ optimizer = torch.optim.Adam(net.parameters(), lr=run_params.run['learning_rate_
 scheduler = StepLR(optimizer, step_size=run_params.run['learning_rate_decay_step_size'],
                    gamma=run_params.run['learning_rate_decay'])
 
-custom_loss = False
+scaled_loss = False
 if run_params.run['loss_function'] == 1:
-    loss_fn = torch.nn.L1Loss()
+    loss_fn = nn_custom.L1Loss()
 elif run_params.run['loss_function'] == 2:
-    loss_fn = nn_custom.GaussianLogLikelihoodLoss(run_params.loss_function_kwargs()['uncertainty_loss_eps'])
+    loss_fn = nn_custom.GaussianLogLikelihoodLoss(**run_params.run['loss_kwargs'])
 elif run_params.run['loss_function'] == 3:
-    loss_fn = nn_custom.MyLoss()
+    custom_loss = True
+    loss_fn = nn_custom.ScaledLoss(**run_params.run['loss_kwargs'])
+    scaled_loss = True
 elif run_params.run['loss_function'] == 4:
-    loss_fn = nn_custom.DivergenceFreeLoss(**run_params.loss_function_kwargs())
+    loss_fn = nn_custom.DivergenceFreeLoss(**run_params.run['loss_kwargs'])
 elif run_params.run['loss_function'] == 5:
-    loss_fn = nn_custom.VelocityGradientLoss(**run_params.loss_function_kwargs())
+    loss_fn = nn_custom.VelocityGradientLoss(**run_params.run['loss_kwargs'])
 else:
-    loss_fn = torch.nn.MSELoss()
+    loss_fn = nn_custom.MSELoss()
 
 # save the model parameter in the beginning
 run_params.save(model_dir)
@@ -149,7 +162,7 @@ net = nn_custom.train_model(net, trainloader, validationloader, scheduler, optim
                        run_params.run['plot_every_n_batches'], run_params.run['save_model_every_n_epoch'],
                        run_params.run['save_params_hist_every_n_epoch'], run_params.run['minibatch_epoch_loss'],
                        run_params.run['compute_validation_loss'], model_dir, args.use_writer,
-                       predict_uncertainty, uncertainty_train_mode, custom_loss)
+                       predict_uncertainty, uncertainty_train_mode,scaled_loss, warm_start_epoch)
 
 # save the model if requested
 if (run_params.run['save_model']):
@@ -167,11 +180,12 @@ if (run_params.run['evaluate_testset']):
         for data in testloader:
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
+            terrain = inputs[:, 0, :, :, :].unsqueeze(1)
             outputs = net(inputs)
-            if run_params.run['loss_function']:
+            if scaled_loss:
                 loss += loss_fn(outputs, labels, inputs)
             else:
-                loss += loss_fn(outputs, labels)
+                loss += loss_fn(outputs, labels, terrain)
 
         print('INFO: Average loss on test set: %s' % (loss.item()/len(testset)))
 