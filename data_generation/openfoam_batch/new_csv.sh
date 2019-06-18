8c8dd132ecb30648ecf33fc17912c2925406299d
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2019-06-18 18:26:11 +0200

    Add unconverged option to make_cscs

--- data_generation/openfoam_batch/new_csv.sh
+++ data_generation/openfoam_batch/new_csv.sh
@@ -1,6 +1,61 @@
 #!/bin/bash
 
-homedir=$(pwd)
+# Set up some stuff for getopts
+OPTIND=1
+home_dir=$(pwd)
+version=2
+
+usage() {
+    echo -e "Usage: $0 [OPTIONS] DIR0 DIR1 ..."
+    echo -e "  DIR0 DIR1 ... Case directories"
+    echo -e "  -v [2, 3]\n\tCSV version (2 default)"
+    echo -e "  -h\n\tPrint this help and exit"
+}
+
+while getopts "v:h" opt; do
+    case "$opt" in
+        v)  version=$OPTARG ;;
+        h|*)  usage
+            exit 0
+            ;;
+    esac
+done
+shift $(expr $OPTIND - 1 )
+
+if [ "$#" -lt 1 ]; then
+    echo "ERROR: No case directories specified" >&2
+    usage >&2
+    exit 1
+fi
+
+if [ "$version" -eq 2 ]; then
+    tminy="200.0"
+    tmaxy="1300.0"
+    tmaxz="1100.0"
+    tsx="86"
+    tsy="63"
+    tsz="95"
+elif [ "$version" -eq 3 ]; then
+    tminy="0.0"
+    tmaxy="1498.0"
+    tmaxz="1100.0"
+    tsx="90"
+    tsy="90"
+    tsz="95"
+elif [ "$version" -eq 4 ]; then
+    tminy="0.0"
+    tmaxy="1498.0"
+    tmaxz="1100.0"
+    tsx="63"
+    tsy="63"
+    tsz="63"
+else
+    echo "ERROR: Version number not recognised" >&2
+    exit 1
+fi
+
+echo -n "Generating terrainDict for V${version}, y=${tminy}:${tmaxy}, zmax=${tmaxz}"
+echo " nx ny nz : $tsx $tsy $tsz"
 
 for indir in "$@"
 do
@@ -8,13 +63,6 @@ do
     sed -i '' "$tdict"
 
     echo "$tdict"
-    tminy="200.0"
-    tmaxy="1300.0"
-    tmaxz="1100.0"
-
-    tsx="86"
-    tsy="63"
-    tsz="95"
 
     foamDictionary -entry 'TERRAIN_DICT:MINY' -set $tminy $tdict
     foamDictionary -entry 'TERRAIN_DICT:MAXY' -set $tmaxy $tdict
