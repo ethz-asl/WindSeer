ab9132e7e0cf50630e994b3676acd8a01b5e1fde
Bogdan Danciu <danciub@student.ethz.ch>
2020-02-28 13:19:00 +0100

    Add the dryden routines for generating turbulence

--- wind_analysis/analysis_utils/generate_turbulence.py
+++ wind_analysis/analysis_utils/generate_turbulence.py
@@ -1,5 +1,6 @@
 import numpy as np
-import math
+import time
+import control
 
 
 def karman_E(k, L, sigma):
@@ -20,10 +21,11 @@ def spec_tens_iso_inc(k, L, sigma):
 
     E_k = karman_E(k, L, sigma)
     phi_mat = [[phi_11, phi_12, phi_13], [phi_21, phi_22, phi_23], [phi_31, phi_32, phi_33]]
-    Phi = E_k / (4 * math.pi * np.linalg.norm(k)**4) * np.array(phi_mat)
+    Phi = E_k / (4 * np.pi * np.linalg.norm(k)**4) * np.array(phi_mat)
     return Phi
 
-def generate_turbulence():
+
+def generate_turbulence_spectral():
     '''
     Prototyping Turbulent Wind Fields based on Spectral Domain Simulation
 
@@ -38,12 +40,13 @@ def generate_turbulence():
               - Simulation of Three-Dimensional Turbulent Velocity Fields,
               R. Frehlich & L.Cornman, J. of applied Meteorology, vol.40, 2000
     '''
-    use_fft = True
+    t_start = time.time()
+    use_fft = False
     lambda_min = 10
 
-    x_range = [0, 15]  # x-grid range [m] (north)
-    y_range = [0, 15]  # y-grid range [m] (east)
-    z_range = [0, 15]  # z-grid range [m] (down)
+    x_range = [0, 1]  # x-grid range [m] (north)
+    y_range = [0, 1]  # y-grid range [m] (east)
+    z_range = [0, 1]  # z-grid range [m] (down)
 
     dx = 0.5
     dy = 0.5
@@ -64,15 +67,15 @@ def generate_turbulence():
 
 
     ### Spectral parameters
-    nk = 51
+    nk = 5
     nk_x = nk
     nk_y = nk
     nk_z = nk
 
     # assemble spatial frequency components (wave vector)
-    k_x = 2*math.pi/lambda_min/nk_x * np.array(np.arange(-(nk_x-1)/2, (nk_x+1)/2).tolist())
-    k_y = 2*math.pi/lambda_min/nk_x * np.array(np.arange(-(nk_y-1)/2, (nk_y+1)/2).tolist())
-    k_z = 2*math.pi/lambda_min/nk_x * np.array(np.arange(-(nk_z-1)/2, (nk_z+1)/2).tolist())
+    k_x = 2*np.pi/lambda_min/nk_x * np.array(np.arange(-(nk_x-1)/2, (nk_x+1)/2).tolist())
+    k_y = 2*np.pi/lambda_min/nk_x * np.array(np.arange(-(nk_y-1)/2, (nk_y+1)/2).tolist())
+    k_z = 2*np.pi/lambda_min/nk_x * np.array(np.arange(-(nk_z-1)/2, (nk_z+1)/2).tolist())
 
     # frequency spacing
     dk_x = k_x[1] - k_x[0]
@@ -103,8 +106,8 @@ def generate_turbulence():
                     E = karman_E(k, L, sigma)
                     E_ij[ikx, iky, ikz] = E
 
-                    E_sum = E_sum + E / (np.linalg.norm(k) ** 2 * 4 * math.pi) * dk_x * dk_y * dk_z
-                    A_ij = np.sqrt(E / (4 * math.pi)) / (np.linalg.norm(k) ** 2) * np.array([[0, k[2], -k[1]],
+                    E_sum = E_sum + E / (np.linalg.norm(k) ** 2 * 4 * np.pi) * dk_x * dk_y * dk_z
+                    A_ij = np.sqrt(E / (4 * np.pi)) / (np.linalg.norm(k) ** 2) * np.array([[0, k[2], -k[1]],
                                                                                              [- k[2], 0, k[0]],
                                                                                              [k[1], -k[0], 0]])
 
@@ -131,7 +134,7 @@ def generate_turbulence():
                             for ikz in range(nk_z):
                                 k = np.array([k_x[ikx], k_y[iky], k_z[ikz]])
                                 k = np.transpose(k)
-                                v_r = v_r + math.exp(1j * np.array(np.transpose(k).dot(r))) \
+                                v_r = v_r + np.exp(1j * np.array(np.transpose(k).dot(r))) \
                                       * np.array(C_ij[:, :, ikx, iky, ikz]).dot(np.array(xi[:, ikx, iky, ikz]))
 
                     U[ipx, ipy, ipz] = v_r[0]
@@ -193,4 +196,138 @@ def generate_turbulence():
         Y = Y2
         Z = Z2
 
+    print('Time for 1 run: ', time.time()-t_start)
     return U, V, W, X, Y, Z
+
+
+def generate_turbulence_dryden(h=20, V=15, b=3, u_20=10, n=10, dt=0.5):
+    """
+    % FUNCTION:		dryden_TF
+    %
+    % PURPOSE:		Generate continuous turbulence estimate from Dryden PSD for
+    %				low altitude (<1000 ft) flight.
+    %
+    % SYNTAX:		[UVW, PQR] = dryden_TF(h, V, b, u_20, n, dt)
+    %
+    % INPUTS:		h		- altitude (m)
+    %				V		- airspeed (m/s)
+    %				b		- wing span (m)
+    %				u_20	- mean wind speed at 20ft (m/s)
+    %							Light turbulence: 7.617 m/s
+    %							Moderate turbulence: 15.234 m/s
+    %							Severe turbulence: 30.468 m/s
+    %				n		- number of time steps
+    %				dt		- time step increment (s)
+    %
+    % OUTPUTS:		UVW		- matrix of turbulence magnitudes (m/s) [n×3]
+    %				PQR		- matrix of rotational turbulence (rad/s) [n×3]
+    %
+    % AUTHOR:		Nicholas Lawrance
+    %
+    % CREATED:		July 2010
+    %
+    % MODIFIED:     July 2010
+    """
+    # Units to Imperial(ft, lb, s)
+    u_20 = u_20 * 3.281
+    h = h * 3.281
+    b = b * 3.281
+    V = V * 3.281
+
+    # Low altitude model
+
+    # Turbulence scale lengths
+    L_u = h / (0.177 + 0.000823 * h) ** 1.2
+    L_v = L_u
+    L_w = h
+
+    # Turbulence intensities
+    sigma_w = 0.1 * u_20
+    sigma_u = sigma_w / (0.177 + 0.000823 * h) ** 0.4
+    sigma_v = sigma_u
+
+    # Dryden turbulence TFs
+    s = control.tf('s')
+    dryden_uTF = sigma_u * np.sqrt(2 * L_u / (np.pi * V)) / (1 + s * L_u / V)
+    dryden_pTF = sigma_w * np.sqrt(0.8 / V) * (np.pi / (4 * b)) ** (1 / 6) /\
+                 (L_w ** (1 / 3) * (1 + s * 4 * b / np.pi / V))
+
+    dryden_vTF = sigma_v * np.sqrt(L_v / np.pi / V) * (1 + s * np.sqrt(3) * L_v / V) / (1 + s * L_v / V) ** 2
+    dryden_rTF = s / V / (1 + s * 3 * b / (np.pi * V)) * dryden_vTF
+
+    dryden_wTF = sigma_w * np.sqrt(L_w / np.pi / V) * (1 + s * np.sqrt(3) * L_w / V) / (1 + s * L_w / V) ** 2
+    dryden_qTF = s / V / (1 + s * 4 * b / (np.pi * V)) * dryden_wTF
+
+    # Convert to state space
+    [num, den] = control.tfdata(dryden_uTF)
+    sys = control.tf2ss(num, den)
+    Au = sys.A; Bu = sys.B; Cu = sys.C; Du = sys.D
+
+    [num, den] = control.tfdata(dryden_vTF)
+    sys = control.tf2ss(num, den)
+    Av = sys.A; Bv = sys.B; Cv = sys.C; Dv = sys.D
+
+    [num, den] = control.tfdata(dryden_wTF)
+    sys = control.tf2ss(num, den)
+    Aw = sys.A; Bw = sys.B; Cw = sys.C; Dw = sys.D
+
+    [num, den] = control.tfdata(dryden_pTF)
+    sys = control.tf2ss(num, den)
+    Ap = sys.A; Bp = sys.B; Cp = sys.C; Dp = sys.D
+
+    [num, den] = control.tfdata(dryden_qTF)
+    sys = control.tf2ss(num, den)
+    Aq = sys.A; Bq = sys.B; Cq = sys.C; Dq = sys.D
+
+    [num, den] = control.tfdata(dryden_rTF)
+    sys = control.tf2ss(num, den)
+    Ar = sys.A; Br = sys.B; Cr = sys.C; Dr = sys.D
+
+    # Generate time series
+    rr = np.pi * np.random.randn(6, n)
+
+    # These are the internal state representations, need to be stored but not
+    # human useful, they are transfered through C and D to get actual gust values
+    xxu = np.zeros((Au.shape[0], 1))
+    xxv = np.zeros((Av.shape[0], 1))
+    xxw = np.zeros((Aw.shape[0], 1))
+    xxp = np.zeros((Ap.shape[0], 1))
+    xxq = np.zeros((Aq.shape[0], 1))
+    xxr = np.zeros((Ar.shape[0], 1))
+
+    # Prealocate gusts
+    UVW = np.zeros((n, 3))
+    PQR = np.zeros((n, 3))
+
+    # Numerical integration
+    for ii in range(n):
+        # Newton lazy integration
+        xxu = xxu + (Au * xxu + Bu * rr[0, ii]) * dt
+        UVW[ii, 0] = Cu * xxu
+
+        xxv = xxv + (Av * xxv + Bv * rr[1, ii]) * dt
+        UVW[ii, 1] = Cv * xxv
+
+        xxw = xxw + (Aw * xxw + Bw * rr[2, ii]) * dt
+        UVW[ii, 2] = Cw * xxw
+
+        xxp = xxp + (Ap * xxp + Bp * rr[3, ii]) * dt
+        PQR[ii, 0] = Cp * xxp
+
+        xxq = xxq + (Aq * xxq + Bq * rr[4, ii]) * dt
+        PQR[ii, 1] = Cq * xxq
+
+        xxr = xxr + (Ar * xxr + Br * rr[5, ii]) * dt
+        PQR[ii, 2] = Cr * xxr
+
+    UVW = UVW / 3.281
+
+    return UVW, PQR
+
+
+# # Test
+t_start = time.time()
+for i in range(5):
+    # u,v,w,x,y,z = generate_turbulence_spectral()
+    uvw, pqr = generate_turbulence_dryden(n=64**2)
+    print('Time per function call: {0}'.format(time.time()-t_start))
