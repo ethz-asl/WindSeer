Fix for Gaussian process regression and add option to subtract average wind loss when optimizing the window variables

wind_analysis/analysis_utils/interpolate_log_data.py
@@ -266,7 +266,7 @@ class UlogInterpolation:
 
     def interpolate_log_data_gpr(self):
         '''
-        Create a wind map from the wind measurements using Gaussian Process Regresion
+        Create a wind map from the wind measurements using Gaussian Process Regression
         for interpolation.
         Compute the mean velocity and variance at the center of each bin by
         evaluating the wind map.
@@ -288,7 +288,7 @@ class UlogInterpolation:
         gp_y.fit(np.column_stack([self._wind_data['alt'], self._wind_data['y'], self._wind_data['x']]),
                  self._wind_data['we'])
         gp_z.fit(np.column_stack([self._wind_data['alt'], self._wind_data['y'], self._wind_data['x']]),
-                 -self._wind_data['wn'])
+                 -self._wind_data['wd'])
 
         # Initialize empty wind, variance and predicted_log_data
         if self._grid_dimensions is not None:
@@ -319,10 +319,10 @@ class UlogInterpolation:
             if not pts:
                 predicted_log_data = [pts, pts, pts]
             else:
-                predicted_log_data_x = gp_x.predict(np.array(pts))
-                predicted_log_data_y = gp_y.predict(np.array(pts))
-                predicted_log_data_z = gp_z.predict(np.array(pts))
-                predicted_log_data = [predicted_log_data_x, predicted_log_data_y, predicted_log_data_z]
+                predicted_log_data_x = gp_x.predict(np.row_stack(pts))
+                predicted_log_data_y = gp_y.predict(np.row_stack(pts))
+                predicted_log_data_z = gp_z.predict(np.row_stack(pts))
+                predicted_log_data = [predicted_log_data_x, predicted_log_data_y, -predicted_log_data_z]
 
         else:  # bin data
             for i in range(len(self._bin_x_coord)):
@@ -345,7 +345,6 @@ class UlogInterpolation:
                 wind[2, self._idx_z[i], self._idx_y[i], self._idx_x[i]] = mean_z.item()
                 variance[2, self._idx_z[i], self._idx_y[i], self._idx_x[i]] = var_z.item()
 
-
         print('GPR interpolation is done [{:.2f} s]'.format(time.time() - t_start))
         return wind, variance, predicted_log_data
 
@@ -356,11 +355,11 @@ class UlogInterpolation:
         '''
 
         interpolating_function_x = RGI((self._terrain.z_terr, self._terrain.y_terr, self._terrain.x_terr),
-                                       inferred_wind_data[0, :].detach().cpu().numpy(), method='nearest')
+                                       inferred_wind_data[0, :].detach().cpu().numpy(), method='linear')
         interpolating_function_y = RGI((self._terrain.z_terr, self._terrain.y_terr, self._terrain.x_terr),
-                                       inferred_wind_data[1, :].detach().cpu().numpy(), method='nearest')
+                                       inferred_wind_data[1, :].detach().cpu().numpy(), method='linear')
         interpolating_function_z = RGI((self._terrain.z_terr, self._terrain.y_terr, self._terrain.x_terr),
-                                       inferred_wind_data[2, :].detach().cpu().numpy(), method='nearest')
+                                       inferred_wind_data[2, :].detach().cpu().numpy(), method='linear')
 
         # Initialize empty list of points where the wind is interpolated
         pts = []
@@ -377,5 +376,5 @@ class UlogInterpolation:
         interpolated_log_data_x = interpolating_function_x(pts)
         interpolated_log_data_y = interpolating_function_y(pts)
         interpolated_log_data_z = interpolating_function_z(pts)
-        interpolated_log_data = [interpolated_log_data_x, interpolated_log_data_y, interpolated_log_data_z]
+        interpolated_log_data = [interpolated_log_data_x, interpolated_log_data_y, -interpolated_log_data_z]
         return interpolated_log_data

wind_analysis/wind_optimiser.py
@@ -549,7 +549,7 @@ class WindOptimiser(object):
         wind_zeros = wind.clone()
         wind_zeros[wind_mask] = 0
 
-        #print(' done [{:.2f} s]'.format(time.time() - t_start))
+        # print(' done [{:.2f} s]'.format(time.time() - t_start))
         return wind, variance, wind_zeros.to(self._device), wind_mask.to(self._device)
 
     # --- Add noise to data ---
@@ -898,21 +898,31 @@ class WindOptimiser(object):
                                     torch.Tensor(labels).to(self._device))
             # print('NN loss is: ', nn_loss)
             # interpolated_output = np.resize(np.array(interpolated_output), (3, len(interpolated_output[0])))
-            nn_losses += nn_loss.item()
+
+            # --- Average wind prediction ---
+            average_wind_input = np.array([input_flight_data['wn'], input_flight_data['we'], input_flight_data['wd']])
+            average_wind_output = np.ones_like(labels) * [average_wind_input[0].mean(),
+                                                          average_wind_input[1].mean(),
+                                                          average_wind_input[2].mean()]
+            average_wind_loss = self._loss_fn(torch.Tensor(average_wind_output).to(self._device),
+                                 torch.Tensor(labels).to(self._device))
+
+            nn_losses += (nn_loss.item() - average_wind_loss.item())
+            # nn_losses += nn_loss.item()
             n += 1
 
         average_nn_loss = nn_losses/n
         t1 = time.time()
         print('Time for 1 iteration', t1-t0)
         print('Window: ', window_size, 'Response: ', response_size ,' Step: ', step_size)
-        print('Average NN loss is: ', average_nn_loss)
+        print('Average loss is: ', average_nn_loss)
         # Calculate derivative of loss with respect to window split variables
         return average_nn_loss
 
     def window_split_optimisation(self, n=100):
-        window = 20
-        response = 20
-        step = 40
+        window = 60
+        response = 40
+        step = 100
         window_split_variables = [window, response, step]
 
         # Set up solver
@@ -1064,7 +1074,7 @@ class WindOptimiser(object):
             # split data into input and label based on window variables
             input_flight_data, label_flight_data = \
                 self.sliding_window_split(flight_data, window_size, response_size, i*step_size)
-            if self.flag.test_simulated_data and self.flag.rescale_prediction: # only for simulated trajectory
+            if self.flag.test_simulated_data and self.flag.rescale_prediction:  # only for simulated trajectory
                 label_flight_data['wn'] *= self.scale * self.params.data['ux' + '_scaling']
                 label_flight_data['we'] *= self.scale * self.params.data['uy' + '_scaling']
                 label_flight_data['wd'] *= self.scale * self.params.data['uz' + '_scaling']
@@ -1082,9 +1092,10 @@ class WindOptimiser(object):
                     valid_labels.append([label_flight_data['wn'][j], label_flight_data['we'][j],
                                          label_flight_data['wd'][j]])
             if len(valid_labels) == 0:
-                labels = []
-            else:
-                labels = np.row_stack(valid_labels)
+                print(i, 'Labels outside terrain dimensions')
+                print('')
+                continue
+            labels = np.row_stack(valid_labels)
             # labels = np.array([label_flight_data['wn'], label_flight_data['we'], label_flight_data['wd']])
 
             # --- Network prediction ---
@@ -1123,10 +1134,6 @@ class WindOptimiser(object):
 
             # --- Average wind prediction ---
             average_wind_input = np.array([input_flight_data['wn'], input_flight_data['we'], input_flight_data['wd']])
-            if len(valid_labels) == 0:
-                print(i, 'Labels outside terrain dimensions')
-                print('')
-                continue
             average_wind_output = np.ones_like(labels) * [average_wind_input[0].mean(),
                                                           average_wind_input[1].mean(),
                                                           average_wind_input[2].mean()]
@@ -1137,14 +1144,14 @@ class WindOptimiser(object):
                 FlightInterpolator = UlogInterpolation(input_flight_data, terrain=self.terrain, predict=True,
                                                        wind_data_for_prediction=label_flight_data)
                 _, _, predicted_output = FlightInterpolator.interpolate_log_data_krigging()
-                krigging_output = np.resize(np.array(predicted_output), (len(predicted_output[0]), 3))
+                krigging_output = np.column_stack(predicted_output)
 
             # --- Interpolation (Gaussian Process Regression) prediction ---
             if self.flag.use_gpr_prediction:
                 FlightInterpolator = UlogInterpolation(input_flight_data, terrain=self.terrain, predict=True,
                                                        wind_data_for_prediction=label_flight_data)
                 _, _, predicted_output = FlightInterpolator.interpolate_log_data_gpr()
-                gpr_output = np.resize(np.array(predicted_output), (len(predicted_output[0]), 3))
+                gpr_output = np.column_stack(predicted_output)
 
             # losses
             nn_loss = self._loss_fn(torch.Tensor(nn_output).to(self._device),