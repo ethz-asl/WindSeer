5b9d26faf756020d1e029765266512b45726fdb5
Florian Achermann <florian.achermann@mavt.ethz.ch>
2021-01-08 19:40:38 +0100

    Fix error with input device in derive

--- wind_prediction/nn_wind_prediction/utils/derivation.py
+++ wind_prediction/nn_wind_prediction/utils/derivation.py
@@ -20,7 +20,7 @@ def derive(input_tensor, deriv_axis, ds=1, terrain=None, no_nans=True):
     if terrain is not None:
         is_terrain = 1 - terrain.sign()
         input_tensor = input_tensor.clone()
-        input_tensor[is_terrain.bool()] = torch.tensor(float('nan'))
+        input_tensor[is_terrain.bool()] = torch.tensor(float('nan')).to(input_tensor.device)
 
     # Permute axes depending on deriv_axs, no permutation required for X
     if deriv_axis == 1:  # Z
