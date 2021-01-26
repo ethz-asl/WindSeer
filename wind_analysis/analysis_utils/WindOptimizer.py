c7137c1b4b06d09b5460c920ceee65d048e9f7a5
Florian Achermann <florian.achermann@mavt.ethz.ch>
2021-01-26 13:45:24 +0100

    Adding verbose output in the WindOptimizer

--- wind_analysis/analysis_utils/WindOptimizer.py
+++ wind_analysis/analysis_utils/WindOptimizer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 import torch
 
 from .utils import get_optimizer, predict
@@ -27,16 +28,33 @@ class WindOptimizer(object):
         gradients = []
         parameter = []
 
+        if config['run']['verbose']:
+            print('Starting optimization')
+
+        if config['run']['masked_loss']:
+            measurements_masked = torch.masked_select(measurements, mask[0] > 0)
+        else:
+            measurements_masked = measurements
+
         while (iter < config['run']['max_iter']) and (loss_difference > config['run']['loss_change_threshold']) and (max_gradient > config['run']['grad_threshold']):
+            if config['run']['verbose']:
+                sys.stdout.write("\rIteration %i" % iter)
+                sys.stdout.flush()
+
             optimizer.zero_grad()
 
             input_vel = generate_input_fn(params, terrain, interpolator).to(device)
             input = torch.cat([terrain, input_vel], dim = 1)
+            input_unscaled = input.clone().detach()
 
             prediction = predict(self.net, input, scale, config['model'])
-            prediction_masked = prediction['pred'] * mask
 
-            loss = self.loss_fn(prediction_masked, measurements)
+            if config['run']['masked_loss']:
+                prediction_masked = torch.masked_select(prediction['pred'], mask[0] > 0)
+            else:
+                prediction_masked = prediction['pred'] * mask
+
+            loss = self.loss_fn(prediction_masked, measurements_masked)
             losses.append(loss.cpu().item())
             if len(losses) > config['run']['loss_change_window'] + 1:
                 loss_difference = np.mean(np.abs(np.diff(losses[-config['run']['loss_change_window'] - 1:])))
@@ -45,11 +63,14 @@ class WindOptimizer(object):
             max_gradient = params.grad.abs().max().cpu().item()
 
             gradients.append(params.grad.detach().cpu().clone())
-            
+
             parameter.append(params.detach().cpu().clone())
 
             optimizer.step()
-            
+
             iter += 1
 
-        return prediction, params, losses, parameter, gradients, input
+        if config['run']['verbose']:
+            print('\nFinished optimization')
+
+        return prediction, params, losses, parameter, gradients, input_unscaled
