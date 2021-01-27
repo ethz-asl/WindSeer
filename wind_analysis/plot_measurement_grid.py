0ad2b8f5c835538ac5e61aa0e191415c31d8b74b
Florian Achermann <florian.achermann@mavt.ethz.ch>
2021-01-27 22:17:56 +0100

    Fix plot_measurement_grid.py

--- wind_analysis/plot_measurement_grid.py
+++ wind_analysis/plot_measurement_grid.py
@@ -4,7 +4,7 @@ import numpy as np
 import nn_wind_prediction.utils as utils
 from analysis_utils import extract_cosmo_data as cosmo
 from analysis_utils import ulog_utils, get_mapgeo_terrain
-from analysis_utils.interpolate_flight_data import FlightInterpolation
+from analysis_utils.bin_log_data import bin_log_data
 import time
 import torch
 
@@ -12,11 +12,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Plot the measured wind during the flight in the same grid as the prediction is made')
     parser.add_argument('input_file', help='Input file, either ulog or hdf5')
     parser.add_argument('-n', '--n_cells', type=int, default=64, help='Number of grid cells in one dimension')
+    parser.add_argument('-b', '--binnning_method', default='binning', help='Method used to bin the flight data into the grid')
     parser.add_argument('-d_hor', '--d_horizontal', type=float, default=1100, help='Horizontal side length of the grid [m]')
     parser.add_argument('-d_ver', '--d_vertical', type=float, default=733, help='Horizontal side length of the grid [m]')
     parser.add_argument('-est', '--estimation', action='store_true', help='In case of the ulog file take the wind from the EKF instead of the postprocessed one')
     parser.add_argument('-cosmo', '--cosmo_file', help='Cosmo file name to determine the grid size')
     parser.add_argument('-tiff', '--geotiff_file', help='Filename of the geotiff file')
+    parser.add_argument('-p', '--predict', action='store_true', help='Predict the full field in case the binning method supports it')
 
     args = parser.parse_args()
 
@@ -60,8 +62,7 @@ if __name__ == "__main__":
     # bin the data into the regular grid
     print('Binning wind data...', end='', flush=True)
     t_start = time.time()
-    UlogInterpolator = FlightInterpolation(wind_data, grid_dimensions)
-    wind, variance = UlogInterpolator.bin_flight_data()
+    wind, variance, measurements_mask, prediction = bin_log_data(wind_data, grid_dimensions, args.binnning_method, True, args.predict)
     print(' done [{:.2f} s]'.format(time.time() - t_start))
 
     # get the terrain data
@@ -79,8 +80,5 @@ if __name__ == "__main__":
         # no terrain
         terrain = torch.ones_like(wind[0])
 
-    print('Plotting the data...', end='', flush=True)
-    t_start = time.time()
-    utils.plot_measurements(wind, variance, terrain)
-    print(' done [{:.2f} s]'.format(time.time() - t_start))
-    plt.show()
+    utils.plot_measurement(['ux', 'uy', 'uz'], wind, measurements_mask,
+                           terrain = terrain, variance = variance, prediction = prediction)
