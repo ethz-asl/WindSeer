6af984967d2d74111f36c1e337fa7120cd7fe745
Florian Achermann <florian.achermann@mavt.ethz.ch>
2021-02-17 13:36:16 +0100

    Remove unnecessary imports

--- wind_analysis/plot_optimizer_results.py
+++ wind_analysis/plot_optimizer_results.py
@@ -1,11 +1,7 @@
 import argparse
 import numpy as np
 
-import nn_wind_prediction.models as models
-import nn_wind_prediction.utils as nn_utils
-from analysis_utils import utils
 from analysis_utils.plotting_analysis import plot_optimizer_results
-from analysis_utils.WindOptimizer import WindOptimizer
 
 parser = argparse.ArgumentParser(description='Visualize the results of the optimizer')
 parser.add_argument('file', help='File containing the results')
