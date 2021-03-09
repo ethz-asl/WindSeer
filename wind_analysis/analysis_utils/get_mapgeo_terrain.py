Fix the distance field calculation for real terrains extracted from geotiffs

wind_analysis/analysis_utils/get_mapgeo_terrain.py
@@ -17,6 +17,7 @@ except ImportError:
 
 try:
     from scipy.interpolate import RectBivariateSpline
+    from scipy import ndimage
 except ImportError:
     print("'scipy' is not installed. Use the command below to install it:")
     print("     pip3 install scipy")
@@ -34,7 +35,8 @@ def plot_terrain_patch(x_img, y_img, z_img, x_p, y_p, z_p):
     a[0].add_patch(Rectangle((x_p[0], y_p[0]), (x_p[-1]-x_p[0]), (y_p[-1]-y_p[0]), edgecolor='r', alpha=1, facecolor='none'))
 
 
-def get_terrain(terrain_geotiff, x, y, z=None, resolution=(64,64,64), build_binary_block=True, plot=False):
+def get_terrain(terrain_geotiff, x, y, z=None, resolution=(64,64,64), build_block=True, plot=False, distance_field=False,
+                horizontal_overflow = 0, return_is_wind = False):
     """
     Extract a terrain patch from a geotiff, and sample to specified resolution
     Inputs:
@@ -46,13 +48,18 @@ def get_terrain(terrain_geotiff, x, y, z=None, resolution=(64,64,64), build_bina
                   Single value: use the minimum terrain height to min+ the value (i.e. specify block height)
                   Two values: use the specified height range
         resolution:         output target resolution (rx, ry, rz)
-        build_binary_block: flag to output full_block
+        build_block: flag to output full_block
+        plot: Plot the extracted terrain
+        distance_field: Return the full_block as a distance field instead of binary map
+        horizontal_overflow: Extract a larger portion of the map, build the distance field and then extract the actual extent.
+                             It is forced to be an integer and the unit is in number of cells.
+        return_is_wind: In case of a binary full_block the cells will be true where there is wind, else terrain cells are true
 
     Outputs:
           x_, y_, z_terr:    regular, monotonic index arrays for the h_terr and full_block arrays
           h_terr:            terrain height array (actual height values in m, resolution[0] x resolution[1]
-          full_block:        binary terrain array (size is resolution input argument)
-                            True is terrain, False is air, dimension order is [z, y, x]
+          full_block:        terrain array (size is resolution input argument) dimension order is [z, y, x].
+                             Depending on the distance_field argument a binary array or a distance field array.
     """
     img = gdal.Open(terrain_geotiff)
 
@@ -78,26 +85,71 @@ def get_terrain(terrain_geotiff, x, y, z=None, resolution=(64,64,64), build_bina
         z_img = np.flipud(z_img)
         y_img = y_img[::-1]
 
-    interp_spline = RectBivariateSpline(y_img, x_img, z_img)
-    x_target = x[0] + (x[1] - x[0])/(resolution[0]-1) * np.arange(resolution[0])
-    y_target = y[0] + (y[1] - y[0])/(resolution[1]-1) * np.arange(resolution[1])
+    dx = (x[1] - x[0])/(resolution[0]-1)
+    dy = (y[1] - y[0])/(resolution[1]-1)
+
+    horizontal_overflow = int(horizontal_overflow)
+    bounds_x = x.copy()
+    bounds_y = y.copy()
+    if distance_field and horizontal_overflow > 0:
+        bounds_x[0] -= horizontal_overflow * dx
+        bounds_x[1] += horizontal_overflow * dx
+        bounds_y[0] -= horizontal_overflow * dy
+        bounds_y[1] += horizontal_overflow * dy
+    else:
+        horizontal_overflow = 0
+
+    # extract the relevant image data to speed up the spline building
+    # especially important if we have a high quality terrain
+    extended_bounds_x = [bounds_x[0] - 2 * np.abs(t[1]), bounds_x[1] + 2 * np.abs(t[1])]
+    extended_bounds_y = [bounds_y[0] - 2 * np.abs(t[5]), bounds_y[1] + 2 * np.abs(t[5])]
+    idx_to_keep_x = np.logical_and(x_img < extended_bounds_x[1], x_img > extended_bounds_x[0])
+    idx_to_keep_y = np.logical_and(y_img < extended_bounds_y[1], y_img > extended_bounds_y[0])
+
+    interp_spline = RectBivariateSpline(y_img[idx_to_keep_y],
+                                        x_img[idx_to_keep_x],
+                                        z_img[idx_to_keep_y][:, idx_to_keep_x])
+
+    x_target = bounds_x[0] + dx * np.arange(resolution[0] + 2 * horizontal_overflow)
+    y_target = bounds_y[0] + dy * np.arange(resolution[1] + 2 * horizontal_overflow)
     z_out = interp_spline(y_target, x_target)
 
     if plot:
         plot_terrain_patch(x_img, y_img, z_img, x_target, y_target, z_out)
 
-    if build_binary_block:
+    if build_block:
         # Build binary indicator array
         if z is None:
-            z = [z_out.min(), z_out.ax()]
+            z = [z_out.min(), z_out.max()]
         elif len(z) == 1:
             z = [z_out.min(), z_out.min()+z[0]]
-        z_target = z[0] + (z[1] - z[0])/(resolution[2]-1) * np.arange(resolution[2])
-        full_block = np.zeros(resolution, dtype='bool')     # full_block is dimension order [z, y, x], z_out is [y, x]
+
+        dz = (z[1] - z[0]) / (resolution[2]-1)
+        bounds_z = z.copy()
+
+        # full_block is dimension order [z, y, x], z_out is [y, x]
+        if distance_field:
+            offset_cells_z = int(np.max([np.ceil((bounds_z[0] - z_out.min()) / dz), 0]))
+            bounds_z[0] -= offset_cells_z * dz
+            full_block = np.zeros((resolution[0] + offset_cells_z, z_out.shape[0], z_out.shape[1]), dtype='bool')
+        else:
+            offset_cells_z = 0
+            full_block = np.zeros(resolution, dtype='bool')
+
+        z_target = bounds_z[0] + dz * np.arange(resolution[2] + offset_cells_z)
+
         for yi in range(z_out.shape[0]):
             for xi in range(z_out.shape[1]):
                 height_dex = np.sum(z_target < z_out[yi,xi])
                 full_block[:height_dex, yi, xi] = True
+
+        if distance_field:
+            is_wind = np.logical_not(full_block).astype('float')
+            full_block = ndimage.distance_transform_edt(is_wind)
+            full_block = full_block[offset_cells_z:, horizontal_overflow:-horizontal_overflow, horizontal_overflow:-horizontal_overflow]
+
+        elif return_is_wind:
+            full_block = np.logical_not(full_block).astype('float')
 
         return x_target, y_target, z_target, z_out, full_block
 

wind_analysis/analysis_utils/utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 import os
-from scipy import ndimage
 import torch
 from torch.optim.optimizer import Optimizer
 
@@ -167,18 +166,18 @@ def load_measurements(config, config_model):
                 grid_dimensions['x_min'] -= 0.5 * diff
                 grid_dimensions['x_max'] += 0.5 * diff
 
-        x_terr, y_terr, z_terr, h_terr, full_block = \
+        _, _, _, _, full_block = \
             get_terrain(config['log']['geotiff_file'],
                         [grid_dimensions['x_min'], grid_dimensions['x_max']],
                         [grid_dimensions['y_min'], grid_dimensions['y_max']],
                         [grid_dimensions['z_min'], grid_dimensions['z_max']],
-                        (config['log']['num_cells'], config['log']['num_cells'], config['log']['num_cells']))
+                        (config['log']['num_cells'], config['log']['num_cells'], config['log']['num_cells']),
+                        build_block = True,
+                        return_is_wind = True,
+                        distance_field = config['log']['distance_field'],
+                        horizontal_overflow = config['log']['horizontal_overflow'])
 
-        is_wind = np.logical_not(full_block).astype('float')
-        if config['log']['distance_field']:
-            terrain = torch.from_numpy(ndimage.distance_transform_edt(is_wind).astype(np.float32))
-        else:
-            terrain = torch.from_numpy(is_wind.astype(np.float32))
+        terrain = torch.from_numpy(full_block.astype(np.float32))
 
 
         measurement, variance, mask, prediction = bin_log_data(wind_data, grid_dimensions)