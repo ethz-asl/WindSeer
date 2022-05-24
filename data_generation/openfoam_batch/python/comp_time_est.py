547f56ea60044f1b0d9c062bd82a1c76753eea12
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2022-05-24 20:16:17 +0200

    Added unconverged time calc

--- data_generation/openfoam_batch/python/comp_time_est.py
+++ data_generation/openfoam_batch/python/comp_time_est.py
@@ -17,7 +17,8 @@ def get_clocktime(log_file):
 
 def estimate_times(cases_dir):
 
-    full_times = {}
+    converged_times = {}
+    unconverged_times = {}
     subdirs = os.listdir(cases_dir)
 
     for dir in tqdm(subdirs):
@@ -29,12 +30,13 @@ def estimate_times(cases_dir):
         if not os.path.exists(os.path.join(wdir, "simpleFoam2.err")): continue
 
         error_size = os.path.getsize(os.path.join(wdir, "simpleFoam2.err"))
-        if error_size != 0:
-            continue
 
         t0 = get_clocktime(os.path.join(wdir, "simpleFoam.log"))
         t1 = get_clocktime(os.path.join(wdir, "simpleFoam2.log"))
-        full_times[dir] = [t0, t1]
+        if error_size != 0:
+            unconverged_times[dir] = [t0, t1]
+            continue
+        converged_times[dir] = [t0, t1]
 
         wind = 2
         wdir = os.path.join(cases_dir, dir, 'W{0}'.format(wind))
@@ -42,29 +44,46 @@ def estimate_times(cases_dir):
             if not os.path.exists(os.path.join(wdir, "simpleFoam.err")): break
 
             error_size = os.path.getsize(os.path.join(wdir, "simpleFoam.err"))
-            if error_size != 0: break
             tn = get_clocktime(os.path.join(wdir, "simpleFoam.log"))
-            full_times[dir].append(tn)
+            if error_size != 0:
+                try:
+                    unconverged_times[dir].append(tn)
+                except KeyError:
+                    unconverged_times[dir] = [tn]
+                break
+
+            converged_times[dir].append(tn)
             wind += 1
             wdir = os.path.join(cases_dir, dir, 'W{0}'.format(wind))
 
-    return full_times
+    return converged_times, unconverged_times
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run through a directory and estimate computational times')
     parser.add_argument('cases_dir', type=str, nargs=1, help='Root input directory (with cases as subdirs)')
     args = parser.parse_args()
-    full_times = estimate_times(args.cases_dir[0])
-    t_full = [t for t in full_times.values()]
+    converged_times, unconverged_times = estimate_times(args.cases_dir[0])
+
+    print('Stats for {0}'.format(args.cases_dir[0]))
+
+    print('Converged:')
+    t_full = [t for t in converged_times.values()]
     t_all = np.concatenate(t_full).astype(float)
     w0_times = np.array([(t[0] + t[1]) for t in t_full])
     wn_times = np.concatenate([t[2:] for t in t_full]).astype(float)
 
-    print('Total time for {0} runs: {1:0.2f} hours'.format(len(t_all), t_all.sum()/3600.0))
-    print('Full average time: {0:0.2f} mins'.format(t_all.mean()/60.0))
-    print('W1 average times: {0:0.2f} mins'.format(w0_times.mean() / 60.0))
-    print('W2: average times: {0:0.2f} mins'.format(wn_times.mean() / 60.0))
+    print('Total time for {0} runs: {1:0.2f} hours'.format(len(t_all), np.nansum(t_all)/3600.0))
+    print('Full average time: {0:0.2f} mins'.format(np.nanmean(t_all)/60.0))
+    print('W1 average times: {0:0.2f} mins'.format(np.nanmean(w0_times) / 60.0))
+    print('W2: average times: {0:0.2f} mins'.format(np.nanmean(wn_times) / 60.0))
+
+    print('Unconverged:')
+    t_full = [t for t in unconverged_times.values()]
+    t_all = np.concatenate(t_full).astype(float)
+
+    print('Total time for {0} runs: {1:0.2f} hours'.format(len(t_all), np.nansum(t_all)/3600.0))
+    print('Full average time: {0:0.2f} mins'.format(np.nanmean(t_all)/60.0))
 
 
 
