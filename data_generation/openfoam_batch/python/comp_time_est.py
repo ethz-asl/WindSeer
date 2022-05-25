abf5207b73a5d6a5a274021cc71472dd61223deb
Nicholas Lawrance <nicholas.lawrance@mavt.ethz.ch>
2022-05-25 11:28:02 +0200

    Add comments and time summary

--- data_generation/openfoam_batch/python/comp_time_est.py
+++ data_generation/openfoam_batch/python/comp_time_est.py
@@ -16,32 +16,46 @@ def get_clocktime(log_file):
 
 
 def estimate_times(cases_dir):
-
+    # Extract the final ClockTime value (in s) for each converged run
+    # Return two dictionaries (converged and un.) with keys of the case run name
+    # (e.g. 'batch05_N_11_S15x15'), and then an array of solve times
+    # Note that the first two elements are the two-phase solve for W1, then
+    # element 2 is W2 and so on until convergence failed.
     converged_times = {}
     unconverged_times = {}
     subdirs = os.listdir(cases_dir)
 
     for dir in tqdm(subdirs):
+
+        # Always start with W1 - if it did't converge we didn't try the later
+        # wind speeds
         wdir = os.path.join(cases_dir, dir, 'W1')
         if not os.path.isdir(wdir):
             continue
 
-        # First check for simpleFoam2.err
+        # First check for simpleFoam2.err (this was a file I created)
         if not os.path.exists(os.path.join(wdir, "simpleFoam2.err")): continue
 
+        # If it's an empty file, there was no error and the solution converged
         error_size = os.path.getsize(os.path.join(wdir, "simpleFoam2.err"))
 
+        # Extract the clocktimes. For the W1 case we always did a two-phase
+        # solver, so there are actually two times (add them together for the
+        # total solve time)
         t0 = get_clocktime(os.path.join(wdir, "simpleFoam.log"))
         t1 = get_clocktime(os.path.join(wdir, "simpleFoam2.log"))
         if error_size != 0:
+            # If there was an error, get the two times anyway
             unconverged_times[dir] = [t0, t1]
             continue
         converged_times[dir] = [t0, t1]
 
+        # Now get all the remaining wind speeds
         wind = 2
         wdir = os.path.join(cases_dir, dir, 'W{0}'.format(wind))
         while os.path.isdir(wdir):
-            if not os.path.exists(os.path.join(wdir, "simpleFoam.err")): break
+            if not os.path.exists(os.path.join(wdir, "simpleFoam.err")):
+                continue
 
             error_size = os.path.getsize(os.path.join(wdir, "simpleFoam.err"))
             tn = get_clocktime(os.path.join(wdir, "simpleFoam.log"))
@@ -50,9 +64,8 @@ def estimate_times(cases_dir):
                     unconverged_times[dir].append(tn)
                 except KeyError:
                     unconverged_times[dir] = [tn]
-                break
-
-            converged_times[dir].append(tn)
+            else:
+                converged_times[dir].append(tn)
             wind += 1
             wdir = os.path.join(cases_dir, dir, 'W{0}'.format(wind))
 
@@ -76,7 +89,7 @@ if __name__ == "__main__":
     print('Total time for {0} runs: {1:0.2f} hours'.format(len(t_all), np.nansum(t_all)/3600.0))
     print('Full average time: {0:0.2f} mins'.format(np.nanmean(t_all)/60.0))
     print('W1 average times: {0:0.2f} mins'.format(np.nanmean(w0_times) / 60.0))
-    print('W2: average times: {0:0.2f} mins'.format(np.nanmean(wn_times) / 60.0))
+    print('W2+ average times: {0:0.2f} mins'.format(np.nanmean(wn_times) / 60.0))
 
     print('Unconverged:')
     t_full = [t for t in unconverged_times.values()]
