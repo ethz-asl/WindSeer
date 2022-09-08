5fb5376965707a2fb5c31c7bf2e397a0ba06532a
Florian Achermann <florian.achermann@mavt.ethz.ch>
2022-09-08 09:30:26 +0200

    Remove obsolete test folder

--- test/test_extract_wind.py
+++
@@ -1,6 +0,0 @@
-import nn_wind_prediction.cosmo as cosmo
-
-out = cosmo.extract_cosmo_data('../wind_analysis/data/cosmo-1_ethz_fcst_2018112315.nc', 46.947225, 8.693297, 12,
-                               terrain_file='../wind_analysis/data/cosmo-1_ethz_ana_const.nc')
-
-print('Extraction successful')
