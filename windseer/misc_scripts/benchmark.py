5195cdadf7ef7865ad5780a3499b4cf507f05c00
Florian Achermann <florian.achermann@mavt.ethz.ch>
2022-10-14 15:09:47 +0200

    Update the benchmark script

--- windseer/misc_scripts/benchmark.py
+++ windseer/misc_scripts/benchmark.py
@@ -3,52 +3,59 @@
 from __future__ import print_function
 
 import argparse
-import nn_wind_prediction.models as models
-import nn_wind_prediction.utils as utils
+import windseer.nn.models as models
+import windseer.utils as utils
 import numpy as np
 import time
 import torch
 import os
 import gc
 
-# ----  Default Params --------------------------------------------------------------
-model_name = 'test_model'
-model_version = 'latest'
-# -----------------------------------------------------------------------------------
-
-parser = argparse.ArgumentParser(description='Script to plot a prediction of the network')
-parser.add_argument('-model_name', dest='model_name', default=model_name, help='The model name')
-parser.add_argument('-model_version', dest='model_version', default=model_version, help='The model version')
-parser.add_argument('-g', dest='n_grid', type=int, default=64, help='The number of cells in the grid in each dimension')
-parser.add_argument('-n', dest='n_iter', type=int, default=100, help='The number of forward passes for the benchmarking')
-parser.add_argument('--mp', dest='mixedprecision', action='store_true', help='Mixed precision inference')
-parser.add_argument('--no_gpu', dest='no_gpu', action='store_true', help='Mixed precision inference')
+parser = argparse.ArgumentParser(
+    description='Script to plot a prediction of the network'
+    )
+parser.add_argument(
+    '-model', dest='model_dir', required=True, help='The model directory'
+    )
+parser.add_argument(
+    '-model_version', dest='model_version', default='latest', help='The model version'
+    )
+parser.add_argument(
+    '-g',
+    dest='n_grid',
+    type=int,
+    default=64,
+    help='The number of cells in the grid in each dimension'
+    )
+parser.add_argument(
+    '-n',
+    dest='n_iter',
+    type=int,
+    default=100,
+    help='The number of forward passes for the benchmarking'
+    )
+parser.add_argument(
+    '--mp',
+    dest='mixedprecision',
+    action='store_true',
+    help='Mixed precision inference'
+    )
+parser.add_argument(
+    '--no_gpu', dest='no_gpu', action='store_true', help='Mixed precision inference'
+    )
 
 args = parser.parse_args()
 
-# check if gpu available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 if args.no_gpu:
     device = torch.device("cpu")
 
-# load the model parameter
-params = utils.EDNNParameters('trained_models/' + args.model_name + '/params.yaml')
-
-# load the model and its learnt parameters
-NetworkType = getattr(models, params.model['model_type'])
-net = NetworkType(**params.model_kwargs())
-
-# load state dict
-state_dict = torch.load('trained_models/' + args.model_name + '/' + args.model_version + '.model',
-                                                                            map_location=lambda storage, loc: storage)
-
-# load params
-net.load_state_dict(state_dict)
-net.to(device)
-net.eval()
+net, params = utils.load_model(
+    args.model_dir, args.model_version, None, device, eval=True
+    )
 
 # get input
-input = torch.randn(4,args.n_grid,args.n_grid,args.n_grid).to(device)
+input = torch.randn(4, args.n_grid, args.n_grid, args.n_grid).to(device).unsqueeze(0)
 
 def start_timer():
     gc.collect()
@@ -63,7 +70,7 @@ with torch.no_grad():
         for i in range(args.n_iter):
             torch.cuda.synchronize()
             start_time = start_timer()
-            output = net(input.unsqueeze(0))
+            output = net(input)
             torch.cuda.synchronize()
             end_time = time.time()
             inference_times.append(end_time - start_time)
