f5c70fb812f08fe6b816665af5976f30688e6edc
Florian Achermann <florian.achermann@mavt.ethz.ch>
2022-10-16 13:50:51 +0200

    Format and cleanup generate_prediction_database script

--- windseer/misc_scripts/generate_prediction_database.py
+++ windseer/misc_scripts/generate_prediction_database.py
@@ -2,39 +2,50 @@
 
 from __future__ import print_function
 
+import windseer.data as data
+import windseer.nn as nn_custom
+import windseer.utils as utils
+
 import argparse
-import nn_wind_prediction.data as data
-import nn_wind_prediction.models as nn_models
-import nn_wind_prediction.nn as nn_custom
-import nn_wind_prediction.utils as utils
 import numpy as np
+import os
 import torch
-from torch.utils.data import DataLoader
 
-# ----  Default Params --------------------------------------------------------------
-dataset = 'data/test.hdf5'
-savename = 'prediction.hdf5'
-# -----------------------------------------------------------------------------------
-
-# define here the models
-models = []
-models.append({'name': 'model_1',
-               'version': 'latest',
-               'prediction_level': 10})
-models.append({'name': 'model_2',
-               'version': 'latest',
-               'prediction_level': 10})
-
-parser = argparse.ArgumentParser(description='Script to plot a prediction of the network')
-parser.add_argument('-d', dest='dataset', default=dataset, help='The test dataset')
-parser.add_argument('-save_name', dest='savename', default=savename, help='The name of the prediction database')
+parser = argparse.ArgumentParser(
+    description='Script to plot a prediction of the network'
+    )
+parser.add_argument('-d', dest='dataset', required=True, help='The test dataset')
+parser.add_argument(
+    '-save_name',
+    dest='savename',
+    default='prediction_dataset.hdf5',
+    help='The name of the prediction database'
+    )
+parser.add_argument(
+    '-m',
+    dest='models',
+    required=True,
+    nargs='+',
+    help='Models used to predict (directory, can be multiple models'
+    )
+parser.add_argument(
+    '-v',
+    dest='versions',
+    required=True,
+    nargs='+',
+    help='Model version (requires the same number of entries as models'
+    )
 
 args = parser.parse_args()
+
+models = []
+for m, v in zip(args.models, args.versions):
+    models.append({'name': m, 'version': v, 'prediction_level': 10})
 
 # check if the dataset arguments are consistent
 dataset_kwargs = None
 for item in models:
-    params = utils.EDNNParameters('trained_models/' + item['name'] + '/params.yaml')
+    params = utils.WindseerParams(os.path.join(item['name'], 'params.yaml'))
 
     if dataset_kwargs == None:
         dataset_kwargs = params.Dataset_kwargs()
@@ -47,24 +58,15 @@ for item in models:
 # check if gpu available
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-dataset_kwargs['return_grid_size'] = True
-
 # load dataset
 dataset_kwargs['return_grid_size'] = True
-testset = data.HDF5Dataset(args.dataset, augmentation = False, **dataset_kwargs)
+testset = data.HDF5Dataset(args.dataset, augmentation=False, **dataset_kwargs)
 
 for item in models:
-    params = utils.EDNNParameters('trained_models/' + item['name'] + '/params.yaml')
-    NetType = getattr(nn_models, params.model['model_type'])
-    net = NetType(**params.model_kwargs())
-    net.load_state_dict(torch.load('trained_models/' + item['name'] + '/' + item['version'] + '.model', map_location=lambda storage, loc: storage))
-    net.to(device)
-    net.eval()
-    try:
-        net.set_prediction_level(item['prediction_level'])
-    except AttributeError:
-        pass
+    net, params = utils.load_model(
+        item['name'], item['version'], args.dataset, device, eval=True
+        )
     item['net'] = net
     item['params'] = params
 
-nn_custom.save_prediction_to_database(models, device, params, savename, testset)
+nn_custom.save_prediction_to_database(models, device, params, args.savename, testset)
