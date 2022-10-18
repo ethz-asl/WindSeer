c94e7b1f226a9ccc33cb25c93cdb424fd9c2ba22
Florian Achermann <florian.achermann@mavt.ethz.ch>
2022-10-18 16:57:14 +0200

    Move extract_cosmo_data to windseer/utils

--- windseer/utils/__init__.py
+++ windseer/utils/__init__.py
@@ -1,11 +1,12 @@
 __all__ = [
-    'bin_log_data', 'derivation', 'dict_utils', 'kwargs_handling', 'load_model',
+    'bin_log_data', 'derivation', 'dict_utils', 'extract_cosmo_data', 'kwargs_handling', 'load_model',
     'prediction_error', 'rescale_tensor', 'terrain_factor', 'yaml_tools',
     ]
 
 from .bin_log_data import *
 from .derivation import *
 from .dict_utils import *
+from .extract_cosmo_data import *
 from .kwargs_handling import *
 from .load_model import *
 from .prediction_error import *
