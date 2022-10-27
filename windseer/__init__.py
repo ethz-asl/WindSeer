cefe3d15907a03b528372f63bde9127a30439316
Florian Achermann <florian.achermann@mavt.ethz.ch>
2022-10-27 15:57:25 +0200

    Move evaluation functions to windseer.evaluation subpackage

--- windseer/__init__.py
+++ windseer/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ['data', 'test', 'utils', 'nn']
+__all__ = ['data', 'evaluation', 'test', 'utils', 'nn']
 
 from windseer import data
+from windseer import evaluation
 from windseer import test
 from windseer import utils
 from windseer import nn
---
+++ windseer/evaluation/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['prediction_error', 'sparse_evaluation']
+
+from .prediction_error import *
+from .sparse_evaluation import *
