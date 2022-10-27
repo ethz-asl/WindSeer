7ad6e599c9670e38e652a053567d219296de101e
Florian Achermann <florian.achermann@mavt.ethz.ch>
2022-10-27 16:22:59 +0200

    Fix format for terrain_overlap_check

--- windseer/misc_scripts/terrain_overlap_check.py
+++ windseer/misc_scripts/terrain_overlap_check.py
@@ -6,22 +6,32 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import os
 import pyproj
-import analysis_utils.extract_cosmo_data as cosmo
 from osgeo import gdal
-from analysis_utils.get_mapgeo_terrain import get_terrain
 
-parser = argparse.ArgumentParser(description='Plot the cosmo grid in comparison to the height map')
+import windseer.utils as utils
+
+parser = argparse.ArgumentParser(
+    description='Plot the cosmo grid in comparison to the height map'
+    )
 parser.add_argument('-c', '--cosmo_file', help='Filename of the cosmo file')
 parser.add_argument('-t', '--geotiff_file', help='Filename of the geotiff')
-parser.add_argument('-b', '--plot_blocking', action='store_true', help='Indicates if plt.show should be blocking')
-parser.add_argument('-n_hor', '--n_horizontal', type=int, default=1024, help='Number of vertical cells')
-parser.add_argument('-n_ver', '--n_vertical', type=int, default=512, help='Number of vertical cells')
-
+parser.add_argument(
+    '-b',
+    '--plot_blocking',
+    action='store_true',
+    help='Indicates if plt.show should be blocking'
+    )
+parser.add_argument(
+    '-n_hor', '--n_horizontal', type=int, default=1024, help='Number of vertical cells'
+    )
+parser.add_argument(
+    '-n_ver', '--n_vertical', type=int, default=512, help='Number of vertical cells'
+    )
 
 args = parser.parse_args()
 
 # does not really matter
-cosmo_time = 1
+cosmo_time = 0
 
 # Get terrain tif limits
 img = gdal.Open(args.geotiff_file)
@@ -30,33 +40,38 @@ img = gdal.Open(args.geotiff_file)
 w = img.RasterXSize
 h = img.RasterYSize
 t = img.GetGeoTransform()
-xlims, ylims = [t[0]+10, t[0] + t[1]*w-10], [t[3] + t[5]*h+10, t[3]-10]
+xlims, ylims = [t[0] + 10, t[0] + t[1] * w - 10], [t[3] + t[5] * h + 10, t[3] - 10]
 
-x_terr, y_terr, h_terr = get_terrain(args.geotiff_file, xlims, ylims, [0.0, 1.0],
-                                     (args.n_horizontal, args.n_horizontal, args.n_vertical), build_block=False)
+x_terr, y_terr, h_terr = utils.get_terrain(
+    args.geotiff_file,
+    xlims,
+    ylims, [0.0, 1.0], (args.n_horizontal, args.n_horizontal, args.n_vertical),
+    build_block=False
+    )
 
 # Get COSMO terrain:
-lon, lat, hsurf, hfl = cosmo.read_cosmo_nc_file(args.cosmo_file, ['lon_1', 'lat_1', 'HSURF', 'HFL'])
+lon, lat, hsurf, hfl = utils.read_cosmo_nc_file(
+    args.cosmo_file, ['lon_1', 'lat_1', 'HSURF', 'HFL']
+    )
 
 # convert to output coordinate projection
 proj_WGS84 = pyproj.Proj(proj='latlong', datum='WGS84')
-proj_EGM96 = pyproj.Proj(init="EPSG:4326", geoidgrids="egm96_15.gtx") # init="EPSG:5773",
-proj_CH_1903_LV03 = pyproj.Proj(init="EPSG:21781")  # https://epsg.io/21781
 proj_CH_1903_LV03_SWISSTOPO = pyproj.Proj(init="CH:1903_LV03")
 
-x_grid, y_grid, h_grid = pyproj.transform(proj_WGS84, proj_CH_1903_LV03_SWISSTOPO, lon, lat, hsurf)
+x_grid, y_grid, h_grid = pyproj.transform(
+    proj_WGS84, proj_CH_1903_LV03_SWISSTOPO, lon, lat, hsurf
+    )
 
 # Plot the wind vector estimates
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-# ax.plot(x, y, alt)
 ax.set_xlabel('Easting (m)')
 ax.set_ylabel('Northing (m)')
 ax.set_zlabel('Altitude (m)')
 
 origin = (0.0, 0.0, 0.0)
-X, Y = np.meshgrid(x_terr-origin[0], y_terr-origin[1])
-hs_terr = ax.plot_surface(X, Y, h_terr-origin[2], cmap=cm.terrain)
+X, Y = np.meshgrid(x_terr - origin[0], y_terr - origin[1])
+hs_terr = ax.plot_surface(X, Y, h_terr - origin[2], cmap=cm.terrain)
 
 x_c, y_c, z_c = [], [], []
 for x, y, h in zip(x_grid.flat, y_grid.flat, h_grid.flat):
