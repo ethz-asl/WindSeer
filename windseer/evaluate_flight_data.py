68d08d4bc0e2ebe1197186e46f7a2bdebb798e3b
Florian Achermann <florian.achermann@mavt.ethz.ch>
2022-10-29 13:44:10 +0200

    Combine loiter and along flight path evaluation into one script and fix format

--- windseer/evaluate_flight_data.py
+++ windseer/evaluate_flight_data.py
@@ -3,21 +3,33 @@ import numpy as np
 import random
 import torch
 
-import nn_wind_prediction.models as models
-import nn_wind_prediction.utils as nn_utils
-from analysis_utils import utils
-from analysis_utils.ulog_utils import extract_wind_data, filter_wind_data
-from analysis_utils.sparse_evaluation import evaluate_flight_log
+import windseer.evaluation as eval
+import windseer.plotting as plotting
+import windseer.utils as utils
 
-parser = argparse.ArgumentParser(description='Evaluate the model performance on real flight data using a sliding window')
+parser = argparse.ArgumentParser(
+    description='Evaluate the model performance on real flight data'
+    )
 parser.add_argument('config_yaml', help='Input yaml config')
-parser.add_argument('-model_dir', dest='model_dir', required=True, help='The directory of the model')
-parser.add_argument('-model_version', dest='model_version', default='latest', help='The model version')
+parser.add_argument(
+    '-model_dir', dest='model_dir', required=True, help='The directory of the model'
+    )
+parser.add_argument(
+    '-model_version', dest='model_version', default='latest', help='The model version'
+    )
+parser.add_argument(
+    '--paths', action='store_true', help='Plot the flight paths with mayavi'
+    )
+parser.add_argument(
+    '--loiter',
+    action='store_true',
+    help='Evaluate the data using the loiter tools instead of along the flight path'
+    )
 args = parser.parse_args()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-config = nn_utils.BasicParameters(args.config_yaml)
+config = utils.BasicParameters(args.config_yaml)
 
 if config.params['measurements']['type'] != 'log':
     raise ValueError('Only using log data is supported by this script')
@@ -25,53 +37,55 @@ if config.params['measurements']['type'] != 'log':
 # load the NN
 if config.params['evaluation']['compute_baseline']:
     net = None
-
-    config.params['model'] = {}
-    config.params['model']['input_channels'] = ['ux', 'uy', 'uz']
-    config.params['model']['label_channels'] = ['ux', 'uy', 'uz']
-    config.params['model']['autoscale'] = False
+    params = None
 
 else:
-    nn_params = nn_utils.EDNNParameters(args.model_dir + '/params.yaml')
-    NetworkType = getattr(models, nn_params.model['model_type'])
-    net = NetworkType(**nn_params.model_kwargs())
-    state_dict = torch.load(args.model_dir + '/' + args.model_version + '.model',
-                            map_location=lambda storage, loc: storage)
-    net.load_state_dict(state_dict)
-    net.to(device)
-    net.eval()
+    net, params = utils.load_model(
+        args.model_dir, args.model_version, None, device, True
+        )
 
-    if nn_params.data['input_mode'] < 3:
+    if params.data['input_mode'] < 3:
         raise ValueError('Models with an input mode other than 5 are not supported')
 
-    config.params['model'] = {}
-    config.params['model']['input_channels'] = nn_params.data['input_channels']
-    config.params['model']['label_channels'] = nn_params.data['label_channels']
-    config.params['model']['autoscale'] = nn_params.data['autoscale']
-    config.params['model']['grid_size'] = nn_params.model['model_args']['grid_size']
-
-    if 'input_smoothing' in nn_params.data.keys():
-        config.params['model']['input_smoothing'] = nn_params.data['input_smoothing']
-        config.params['model']['input_smoothing_interpolation'] = nn_params.data['input_smoothing_interpolation']
-        config.params['model']['input_smoothing_interpolation_linear'] = nn_params.data['input_smoothing_interpolation_linear']
-    else:
-        config.params['model']['input_smoothing'] = False
-    
-    for key in nn_params.data.keys():
-        if 'scaling' in key:
-            config.params['model'][key] = nn_params.data[key]
+config = utils.update_sparse_config(config, params)
 
 # load the data
-_, terrain, _, _, scale, wind_data, grid_dimensions = utils.load_measurements(config.params['measurements'], config.params['model'])
+data = eval.load_wind_data(config, args.loiter)
 
-wind_data_validation = []
-if not config.params['evaluation']['validation_file'] is None:
-    for val_file in config.params['evaluation']['validation_file']:
-        wd_val = extract_wind_data(val_file, False)
-        if config.params['measurements']['log']['filter_window_size'] > 0:
-                wd_val = filter_wind_data(wd_val, config.params['measurements']['log']['filter_window_size'])
-        wind_data_validation.append(wd_val)
+if args.paths:
+    x_res = (data['grid_dimensions']['x_max'] -
+             data['grid_dimensions']['x_min']) / data['grid_dimensions']['n_cells']
+    y_res = (data['grid_dimensions']['y_max'] -
+             data['grid_dimensions']['y_min']) / data['grid_dimensions']['n_cells']
+    z_res = (data['grid_dimensions']['z_max'] -
+             data['grid_dimensions']['z_min']) / data['grid_dimensions']['n_cells']
+    trajectories = []
+    flight_data = [data['wind_data']]
+    if 'wind_data_validation' in data.keys():
+        flight_data += data['wind_data_validation']
 
-config.params['model']['input_channels'] += ['mask']
+    for fd in flight_data:
+        traj = {
+            'x': (fd['x'] - data['grid_dimensions']['x_min']) / x_res,
+            'y': (fd['y'] - data['grid_dimensions']['y_min']) / y_res,
+            'z': (fd['alt'] - data['grid_dimensions']['z_min']) / z_res
+            }
+        trajectories.append(traj)
 
-evaluate_flight_log(wind_data, scale, terrain, grid_dimensions, net, config, device, wind_data_validation)
+    plotting.mlab_plot_trajectories(
+        trajectories,
+        data['terrain'],
+        terrain_mode='blocks',
+        terrain_uniform_color=False,
+        blocking=False
+        )
+
+if args.loiter:
+    # add the mask to the input channel for the plotting scripts
+    config.params['model']['input_channels'] += ['mask']
+    eval.loiter_evaluation(data, net, config, device, True)
+else:
+    eval.evaluate_flight_log(
+        data['wind_data'], data['scale'], data['terrain'], data['grid_dimensions'], net,
+        config, device, data['wind_data_validation'], False, True
+        )
